6bfd447cca7cb9360fcfdb149fc8d0aeeb71f11f
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-02-20 15:02:02 -0500

    Integrate consent create-edit feature in app module
    #12750 - As a developer, I will have created the architecture and design for angular component-based front end/UI

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,9 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
+
 
 import { AppComponent } from './app.component';
+import {CoreModule} from "./core/core.module";
+import {ConsentModule} from "./consent/consent.module";
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot()
+    CoreModule,
+    ConsentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
