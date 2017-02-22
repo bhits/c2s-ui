49176e17245651264fcff29fc78521bb590ded92
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-02-22 09:59:34 -0500

    Implement dialog component
    #12750 - As a developer, I will have created the architecture and design for angular component-based front end/UI

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DialogComponent } from './dialog/dialog.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [DialogComponent],
+  exports:[DialogComponent]
 })
 export class SharedModule { }
