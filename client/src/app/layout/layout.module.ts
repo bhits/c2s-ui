4f81fcc203c25e644f8e0df9d3be71df11b688f5
Shruti Rao <shruti.rao@feisystems.com>
2017-02-22 14:01:22 -0500

    Create footer component
    #12751:As a developer, I will have created "Layout" angular components for Consent Management UI

--- client/src/app/layout/layout.module.ts
+++ client/src/app/layout/layout.module.ts
@@ -4,11 +4,12 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
 import { LogoComponent } from './logo/logo.component';
 import { MenuComponent } from './menu/menu.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [UserProfileComponent, LogoComponent, MenuComponent, HeaderComponent]
+  declarations: [UserProfileComponent, LogoComponent, MenuComponent, HeaderComponent, FooterComponent]
 })
 export class LayoutModule { }
