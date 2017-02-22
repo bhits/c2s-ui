3ba8b55294843add13fad866ec70bf22cae8174d
JiahaoLi <Jiahao.Li@feisystems.com>
2017-02-22 17:39:00 -0500

    Implement provider address pipe
    #12752: As a developer, I will have created "Provider" angular components for Consent Management UI

---
+++ client/src/app/provider/shared/provider-address.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { ProviderAddressPipe } from './provider-address.pipe';
+
+describe('ProviderAddressPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ProviderAddressPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
