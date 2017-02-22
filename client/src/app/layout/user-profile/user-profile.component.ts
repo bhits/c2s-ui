22a7a05e5833b48b1f4277bc91e4e6195995e08e
Shruti Rao <shruti.rao@feisystems.com>
2017-02-22 13:20:54 -0500

    Create user-profile component
    #12751:As a developer, I will have created "Layout" angular components for Consent Management UI

---
+++ client/src/app/layout/user-profile/user-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'c2s-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css']
+})
+export class UserProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
