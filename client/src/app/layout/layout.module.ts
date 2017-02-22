bfc795741345225c1a4d0c46f407f1455e4b491f
Shruti Rao <shruti.rao@feisystems.com>
2017-02-22 13:59:16 -0500

    Create logo component
    #12751:As a developer, I will have created "Layout" angular components for Consent Management UI

--- client/src/app/layout/layout.module.ts
+++ client/src/app/layout/layout.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [UserProfileComponent]
+  declarations: [UserProfileComponent, LogoComponent]
 })
 export class LayoutModule { }
