236b6288b74e44fae26ecbbe81f8cc327aab003a
Shruti Rao <shruti.rao@feisystems.com>
2017-02-22 13:59:55 -0500

    Create menu component
    #12751:As a developer, I will have created "Layout" angular components for Consent Management UI

--- client/src/app/layout/layout.module.ts
+++ client/src/app/layout/layout.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { LogoComponent } from './logo/logo.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [UserProfileComponent, LogoComponent]
+  declarations: [UserProfileComponent, LogoComponent, MenuComponent]
 })
 export class LayoutModule { }
