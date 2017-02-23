a57ed8f86ddc984666fa35c92fca57ff0a843c59
JiahaoLi <Jiahao.Li@feisystems.com>
2017-02-23 15:27:29 -0500

    Edit provider search html
    #12752: As a developer, I will have created "Provider" angular components for Consent Management UI

--- client/src/app/provider/provider-list/provider-list.component.ts
+++ client/src/app/provider/provider-list/provider-list.component.ts
@@ -3,7 +3,6 @@ import {Provider} from "../shared/provider.model";
 import {ProviderService} from "../shared/provider.service";
 
 @Component({
-  selector: 'c2s-provider-list',
   templateUrl: './provider-list.component.html',
   styleUrls: ['./provider-list.component.css']
 })
--- client/src/app/provider/provider-search/provider-search.component.ts
+++ client/src/app/provider/provider-search/provider-search.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 
 @Component({
-  selector: 'c2s-provider-search',
   templateUrl: './provider-search.component.html',
   styleUrls: ['./provider-search.component.css']
 })
 export class ProviderSearchComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
