85fe09b9612a773b00aa92148c2347deb08675e1
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-02-23 13:57:53 -0500

    Implement consent service lookup
    #12750 - As a developer, I will have created the architecture and design for angular component-based front end/UI

--- client/src/app/consent/consent.service.ts
+++ client/src/app/consent/consent.service.ts
@@ -1,8 +1,44 @@
 import { Injectable } from '@angular/core';
+import {Http, Response} from "@angular/http";
+import {SensitivityPolicy} from "./sensitivity-policy";
+import 'rxjs/add/operator/toPromise';
+import {PurposeOfUse} from "./purpose-of-use";
+import {Provider} from "./Provider";
 
 @Injectable()
 export class ConsentService {
+  //TODO get configuration url from backend on application startup
+  private pcmBaseUrl:string = "http://localhost/pcm/patients/";
+  private pcmProvidersUrl:string = this.pcmBaseUrl + "/providers";
+  private pcmPurposeOfUseUrl:string = this.pcmBaseUrl + "purposeOfUse";
+  private pcmSensitivityPolicyUrl:string = this.pcmBaseUrl + "sensitivityPolicy";
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  getProviders(): Promise<Provider[]> {
+
+    return this.http.get(this.pcmProvidersUrl)
+                      .toPromise()
+                      .then(response => response.json() as Provider[])
+                      .catch(this.handleError);
+  }
+
+  getPurposeOfUses():Promise<PurposeOfUse[]>{
+    return this.http.get(this.pcmPurposeOfUseUrl)
+                .toPromise()
+                .then(response => response.json() as PurposeOfUse[])
+                .catch(this.handleError);
+  }
+
+  getSensitivityPolices():Promise<SensitivityPolicy[]>{
+    return this.http.get(this.pcmSensitivityPolicyUrl)
+                        .toPromise()
+                        .then(response => response.json() as SensitivityPolicy[])
+                        .catch(this.handleError);
+  }
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
 
 }
