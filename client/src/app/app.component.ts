6a24b4c2ab2cc18355636ac622e23ceb04b0199f
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-02-23 09:36:38 -0500

    Remove sample dialog code
    #12750 - As a developer, I will have created the architecture and design for angular component-based front end/UI

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,15 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  // title = 'app works!';
 
-  showDeleteProviderModal = false;
-
-  deleteProvider(){
-    this.toggleDeleteProviderModal();
-  }
-
-  toggleDeleteProviderModal(){
-    this.showDeleteProviderModal = !this.showDeleteProviderModal;
-  }
+  // showDeleteProviderModal = false;
+  //
+  // deleteProvider(){
+  //   this.toggleDeleteProviderModal();
+  // }
+  //
+  // toggleDeleteProviderModal(){
+  //   this.showDeleteProviderModal = !this.showDeleteProviderModal;
+  // }
 }
