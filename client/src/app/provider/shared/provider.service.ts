8660b628194ad423697839b8b3e08ffd303dd9c7
JiahaoLi <Jiahao.Li@feisystems.com>
2017-02-24 20:17:55 -0500

    Implement search provider service
    #12752: As a developer, I will have created "Provider" angular components for Consent Management UI

--- client/src/app/provider/shared/provider.service.ts
+++ client/src/app/provider/shared/provider.service.ts
@@ -1,25 +1,39 @@
-import {Http} from "@angular/http";
+import {Http, URLSearchParams} from "@angular/http";
 import {Injectable} from "@angular/core";
 import {Provider} from "./provider.model";
 import "rxjs/add/operator/toPromise";
+import {ProviderRequestQuery} from "./provider-request-query.model";
 
 @Injectable()
 export class ProviderService {
-  private baseProvidersUrl = 'http://localhost/pcm/patients/providers';
+  private basePcmUrl = 'http://localhost/pcm/patients/providers';
+  private basePlsUrl = 'http://localhost/pls/providers';
 
   constructor(private http: Http) {
   }
 
   getProviders(): Promise<Provider[]> {
-    return this.http.get(this.baseProvidersUrl)
+    return this.http.get(this.basePcmUrl)
       .toPromise()
       .then(response => response.json() as Provider[])
       .catch(this.handleError);
   }
 
+  searchProviders(requestParams: ProviderRequestQuery): Promise<Provider[]> {
+    const SEARCH_PROVIDERS_URL = this.basePlsUrl + "/search/query";
+
+    let params: URLSearchParams = this.requestParams(requestParams);
+
+    return this.http.get(SEARCH_PROVIDERS_URL, {
+      search: params
+    }).toPromise()
+      .then(response => response.json() as Provider[])
+      .catch(this.handleError);
+  }
+
   deleteProvider(npi: string): Promise<void> {
-    const deleteProvidersUrl = `${this.baseProvidersUrl}/${npi}`;
-    return this.http.delete(deleteProvidersUrl)
+    const DELETE_PROVIDERS_URL = `${this.basePcmUrl}/${npi}`;
+    return this.http.delete(DELETE_PROVIDERS_URL)
       .toPromise()
       .then(() => null)
       .catch(this.handleError);
@@ -28,5 +42,21 @@ export class ProviderService {
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error);
     return Promise.reject(error.message || error);
+  }
+
+  private requestParams(requestParams: ProviderRequestQuery): URLSearchParams {
+    let params: URLSearchParams = new URLSearchParams();
+    params.set('@state', requestParams.state);
+    params.set('@city', requestParams.city);
+    params.set('@zipcode', requestParams.zipcode);
+    params.set('@gender', requestParams.gender);
+    params.set('@phone', requestParams.phone);
+    params.set('@lastname', requestParams.lastname);
+    params.set('@firstname', requestParams.firstname);
+    params.set('@orgname', requestParams.orgname);
+    params.set('projection', requestParams.projection);
+    params.set('page', requestParams.page);
+
+    return params;
   }
 }
