2e1a9feff3f8899b3ae2a02b668ca0186b6c9587
JiahaoLi <Jiahao.Li@feisystems.com>
2017-02-25 21:11:19 -0500

    Introduce ng2-pagination to support automatic pagination
    #12752: As a developer, I will have created "Provider" angular components for Consent Management UI

--- client/src/app/provider/provider-search-result/provider-search-result.component.ts
+++ client/src/app/provider/provider-search-result/provider-search-result.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, Input} from "@angular/core";
 import {ProviderSearchResult} from "../shared/provider-search-result.model";
+import {PaginationInstance} from "ng2-pagination";
 
 @Component({
   selector: 'c2s-provider-search-result',
@@ -8,10 +9,18 @@ import {ProviderSearchResult} from "../shared/provider-search-result.model";
 })
 export class ProviderSearchResultComponent implements OnInit {
   @Input() providerResult: ProviderSearchResult[];
+  paginationConfig: PaginationInstance = {
+    itemsPerPage: 10,
+    currentPage: 1
+  };
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  onPageChange(number: number) {
+    this.paginationConfig.currentPage = number;
   }
 }
--- client/src/app/provider/provider.module.ts
+++ client/src/app/provider/provider.module.ts
@@ -1,22 +1,14 @@
-import {CommonModule} from "@angular/common";
-import {FormsModule} from "@angular/forms";
-import {FlexLayoutModule} from "@angular/flex-layout";
-import {MaterialModule} from "@angular/material";
 import {NgModule} from "@angular/core";
-import {ProviderAddressPipe} from "./shared/provider-address.pipe";
-import {ProviderListComponent} from "./provider-list/provider-list.component";
-import {ProviderService} from "./shared/provider.service";
 import {ProviderRoutingModule} from "./provider-routing.module";
-import {ProviderSearchComponent} from "./provider-search/provider-search.component";
 import {SharedModule} from "../shared/shared.module";
+import {ProviderService} from "./shared/provider.service";
+import {ProviderListComponent} from "./provider-list/provider-list.component";
+import {ProviderSearchComponent} from "./provider-search/provider-search.component";
 import {ProviderSearchResultComponent} from "./provider-search-result/provider-search-result.component";
+import {ProviderAddressPipe} from "./shared/provider-address.pipe";
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    FlexLayoutModule,
-    MaterialModule,
     ProviderRoutingModule,
     SharedModule
   ],
