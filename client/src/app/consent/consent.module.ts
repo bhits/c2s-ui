8490464e27df082596da775b5986f142241ff740
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-02-27 08:46:29 -0500

    Import MD2 module
    #12670 - C2S v2 :: Patient Portal :: PLS :: Search Results Do Not Change When Switching Pages of Resuts

--- client/src/app/consent/consent.module.ts
+++ client/src/app/consent/consent.module.ts
@@ -8,10 +8,12 @@ import { SelectProviderComponent } from './select-provider/select-provider.compo
 import { MedicalInformationComponent } from './medical-information/medical-information.component';
 import { ConsentCreateEditComponent } from './consent-create-edit/consent-create-edit.component';
 import {MaterialModule} from "@angular/material";
+import {Md2Module} from "md2";
 
 @NgModule({
   imports: [
     CommonModule,
+    Md2Module, // TODO verify while MD2 components not working at when module is imported at root modulel
     MaterialModule,
     FormsModule // TODO: verify while FormModule when imported at root level is not available to every other module
   ],
