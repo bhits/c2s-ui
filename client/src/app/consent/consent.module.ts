57f773b043ee433790a7170f7eec09881260e7d3
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-02-27 10:55:20 -0500

    Import sharedModule
    #12670 - C2S v2 :: Patient Portal :: PLS :: Search Results Do Not Change When Switching Pages of Resuts

--- client/src/app/consent/consent.module.ts
+++ client/src/app/consent/consent.module.ts
@@ -1,22 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule  } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {Md2Module} from "md2";
 
 import { ConsentTermsComponent } from './consent-terms/consent-terms.component';
 import { PurposeOfUseComponent } from './purpose-of-use/purpose-of-use.component';
 import { SelectProviderComponent } from './select-provider/select-provider.component';
 import { MedicalInformationComponent } from './medical-information/medical-information.component';
 import { ConsentCreateEditComponent } from './consent-create-edit/consent-create-edit.component';
-import {MaterialModule} from "@angular/material";
-import {Md2Module} from "md2";
 import {ConsentService} from "./consent.service";
+import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
   imports: [
-    CommonModule,
+    SharedModule,
     Md2Module, // TODO verify while MD2 components not working at when module is imported at root modulel
-    MaterialModule,
-    FormsModule // TODO: verify while FormModule when imported at root level is not available to every other module
   ],
   declarations: [
     ConsentTermsComponent,
@@ -26,8 +22,7 @@ import {ConsentService} from "./consent.service";
     ConsentCreateEditComponent
   ],
   exports: [
-    ConsentCreateEditComponent,
-    MaterialModule
+    ConsentCreateEditComponent
   ],
   providers: [ConsentService]
 })
