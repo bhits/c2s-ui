0af8bbaca38afdf14bf3d9dfe91160dd3022121a
JiahaoLi <Jiahao.Li@feisystems.com>
2017-02-27 13:03:49 -0500

    Add links data model
    #12752: As a developer, I will have created "Provider" angular components for Consent Management UI

--- client/src/app/provider/shared/provider-search-response.model.ts
+++ client/src/app/provider/shared/provider-search-response.model.ts
@@ -1,11 +1,13 @@
 import {Page} from "../../shared/page.model";
 import {ProviderProjection} from "./provider-projection.model";
+import {Links} from "../../shared/links.model";
 
 export class ProviderSearchResponse {
   _embedded: EmbeddedResources;
+  _links: Links;
   page: Page;
 }
 
-export class EmbeddedResources {
+interface EmbeddedResources {
   providers: ProviderProjection[];
 }
