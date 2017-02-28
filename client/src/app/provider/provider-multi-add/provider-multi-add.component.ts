f409657bb67c5fae9000bbd9acfbaecef317668d
JiahaoLi <Jiahao.Li@feisystems.com>
2017-02-27 23:29:57 -0500

    Enable to delete providers in waiting list
    #12752: As a developer, I will have created "Provider" angular components for Consent Management UI

--- client/src/app/provider/provider-multi-add/provider-multi-add.component.ts
+++ client/src/app/provider/provider-multi-add/provider-multi-add.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit, Input} from "@angular/core";
+import {Component, OnInit, Input, ViewContainerRef} from "@angular/core";
+import {ConfirmDialogService} from "../../shared/dialog/confirm-dialog.service";
 import {ProviderProjection} from "../shared/provider-projection.model";
 
 @Component({
@@ -8,15 +9,32 @@ import {ProviderProjection} from "../shared/provider-projection.model";
 })
 export class ProviderMultiAddComponent implements OnInit {
   @Input() providers: ProviderProjection[];
+  currentProvider: ProviderProjection = null;
   tHeads = [
     {text: '', cols: 1, color: 'lightgray'},
     {text: 'Name/Facility', cols: 3, color: 'lightgray'}
   ];
 
-  constructor() {
+  constructor(private confirmDialogService: ConfirmDialogService,
+              private viewContainerRef: ViewContainerRef) {
   }
 
   ngOnInit() {
   }
 
+  confirmDeleteProvider(provider: ProviderProjection) {
+    this.confirmDialogService
+      .confirm('Delete Provider', 'Are you sure you want to delete this provider?', this.viewContainerRef)
+      .subscribe(res => {
+        this.deleteProvider(res, provider);
+      });
+  }
+
+  deleteProvider(option: boolean, provider: ProviderProjection) {
+    if (option) {
+      this.currentProvider = provider;
+      this.providers.splice(this.providers.indexOf(this.currentProvider), 1);
+      console.log(this.providers);
+    }
+  }
 }
