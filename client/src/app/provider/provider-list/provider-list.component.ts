819215a0d5f5e9c866d83109d8c1ce9e2d21ffca
JiahaoLi <Jiahao.Li@feisystems.com>
2017-03-01 00:07:05 -0500

    Implement to use MD2 confirm dialog components when delete provider in provider list
    #12752: As a developer, I will have created "Provider" angular components for Consent Management UI

--- client/src/app/provider/provider-list/provider-list.component.ts
+++ client/src/app/provider/provider-list/provider-list.component.ts
@@ -1,5 +1,4 @@
-import {Component, OnInit, ViewContainerRef} from "@angular/core";
-import {ConfirmDialogService} from "../../shared/dialog/confirm-dialog.service";
+import {Component, OnInit} from "@angular/core";
 import {Provider} from "../shared/provider.model";
 import {ProviderService} from "../shared/provider.service";
 import {PaginationInstance} from "ng2-pagination";
@@ -18,9 +17,7 @@ export class ProviderListComponent implements OnInit {
   };
   accordionTab: boolean = true;
 
-  constructor(private providerService: ProviderService,
-              private confirmDialogService: ConfirmDialogService,
-              private viewContainerRef: ViewContainerRef) {
+  constructor(private providerService: ProviderService) {
   }
 
   ngOnInit() {
@@ -32,16 +29,9 @@ export class ProviderListComponent implements OnInit {
     this.paginationConfig.currentPage = number;
   }
 
-  confirmDeleteProvider(provider: Provider) {
-    this.confirmDialogService
-      .confirm('Delete Provider', 'Are you sure you want to delete this provider?', this.viewContainerRef)
-      .subscribe(res => {
-        this.deleteProvider(res, provider);
-      });
-  }
-
-  deleteProvider(option: boolean, provider: Provider) {
-    if (option) {
+  confirmDeleteProvider(dialog: any, provider: Provider) {
+    dialog.close();
+    if (provider != name) {
       this.providerService.deleteProvider(provider.npi)
         .then(() =>
           this.providers = this.providers.filter(p => p !== provider));
