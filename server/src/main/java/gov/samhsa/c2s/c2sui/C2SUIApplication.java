090087c0cabbfb88996f432e6ba3a455ae47b2aa
Shruti Rao <shruti.rao@feisystems.com>
2017-03-01 16:28:23 -0500

    Enable Discovery Client and Config Server
    #12751:As a developer, I will have created "Layout" angular components for Consent Management UI

--- server/src/main/java/gov/samhsa/c2s/c2sui/C2SUIApplication.java
+++ server/src/main/java/gov/samhsa/c2s/c2sui/C2SUIApplication.java
@@ -2,9 +2,21 @@ package gov.samhsa.c2s.c2sui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
+@EnableDiscoveryClient
+@EnableResourceServer
 public class C2SUIApplication {
+
+    @RequestMapping(value = "/fe/**")
+    public String redirect() {
+        return "forward:/";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(C2SUIApplication.class, args);
