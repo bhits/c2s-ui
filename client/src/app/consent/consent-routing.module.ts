f8d1ece2339857cd6141b594baa65e351ad6c7f2
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-03-01 12:46:59 -0500

    Implement basic consent routing                                                                                                                                                                                                                                                                                                                                                #12670 - C2S v2 :: Patient Portal :: PLS :: Search Results Do Not Change When Switching Pages of Resuts

---
+++ client/src/app/consent/consent-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {Routes, RouterModule} from "@angular/router";
+import {ConsentCreateEditComponent} from "./consent-create-edit/consent-create-edit.component";
+
+
+const consentRoutes: Routes = [
+  {path: 'consent-list', component: ConsentCreateEditComponent },
+  {path: 'consent-create-edit', component: ConsentCreateEditComponent}
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(consentRoutes)
+  ],
+  declarations: []
+})
+export class ConsentRoutingModule { }
