27e848468d6b79a5929b4d89f867b307c118130f
JiahaoLi <Jiahao.Li@feisystems.com>
2017-03-04 14:09:50 -0500

    Implement to use conditional form provider type in searching provider form
    #12752: As a developer, I will have created "Provider" angular components for Consent Management UI

--- client/src/app/provider/provider-search/provider-search.component.ts
+++ client/src/app/provider/provider-search/provider-search.component.ts
@@ -1,16 +1,17 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {ProviderService} from "../shared/provider.service";
 import {ProviderRequestQuery} from "../shared/provider-request-query.model";
 import {ProviderSearchResponse} from "../shared/provider-search-response.model";
-import {FormGroup, FormBuilder, Validators} from "@angular/forms";
+import {FormGroup, FormBuilder, Validators, FormControl} from "@angular/forms";
 
 @Component({
   selector: 'c2s-provider-search',
   templateUrl: './provider-search.component.html',
   styleUrls: ['./provider-search.component.css']
 })
-export class ProviderSearchComponent {
+export class ProviderSearchComponent implements OnInit {
   private searchProviderFrom: FormGroup;
+  private PHONE_PATTERN = '^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$';
 
   private searchResponse: ProviderSearchResponse;
   private accordionTab: boolean = true;
@@ -29,40 +30,154 @@ export class ProviderSearchComponent {
     {genderCode: 'F', genderValue: 'Female'}
   ];
 
+  public PROVIDER_TYPE = {
+    INDIVIDUAL: 'individual',
+    ORGANIZATION: 'organization'
+  };
+
   constructor(private formBuilder: FormBuilder,
               private providerService: ProviderService) {
-    this.createForm();
   }
 
-  private createForm() {
+  ngOnInit(): void {
     const ZIP_PATTERN = '^[0-9]{5}(?:-[0-9]{4})?$';
-    const PHONE_PATTERN = '^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$';
     this.searchProviderFrom = this.formBuilder.group({
       state: '',
       city: ['', Validators.minLength(2)],
       zipCode: ['', Validators.pattern(ZIP_PATTERN)],
-      lastName: ['', Validators.minLength(2)],
-      firstName: ['', Validators.minLength(2)],
-      genderCode: '',
-      phone: ['', Validators.pattern(PHONE_PATTERN)],
-      orgName: ['', Validators.minLength(2)]
+      providerType: this.initProviderTypeFormGroup()
+    });
+
+    this.subscribeProviderTypeChanges();
+
+    this.setProviderType(this.PROVIDER_TYPE.INDIVIDUAL);
+  }
+
+  private initProviderTypeFormGroup() {
+    // initialize provider type form group
+    return this.formBuilder.group({
+      type: [''],
+      individual: this.formBuilder.group(this.initProviderIndividualModel()),
+      organization: this.formBuilder.group(this.initProviderOrganizationModel()),
     });
   }
 
-  getFormControl(formControl: string) {
-    return this.searchProviderFrom.get(formControl);
+  private initProviderIndividualModel() {
+    // initialize individual model
+    return {
+      lastName: ['', Validators.minLength(2)],
+      firstName: ['', Validators.minLength(2)],
+      genderCode: '',
+      phone: ['', Validators.pattern(this.PHONE_PATTERN)]
+    };
+  }
+
+  private initProviderOrganizationModel() {
+    // initialize organization model
+    return {
+      orgName: ['', Validators.minLength(2)],
+      phone: ['', Validators.pattern(this.PHONE_PATTERN)]
+    };
+  }
+
+  private subscribeProviderTypeChanges() {
+// controls
+    const ptCtrl = (<any>this.searchProviderFrom).controls.providerType;
+    const individualCtrl = ptCtrl.controls.individual;
+    const organizationCtrl = ptCtrl.controls.organization;
+
+    // initialize value changes stream
+    const typeChanges = ptCtrl.controls.type.valueChanges;
+
+    // subscribe to the stream
+    typeChanges.subscribe(providerType => {
+      if (providerType === this.PROVIDER_TYPE.INDIVIDUAL) {
+        Object.keys(individualCtrl.controls).forEach(key => {
+          individualCtrl.controls[key].setValidators(this.initProviderIndividualModel()[key][1]);
+          individualCtrl.controls[key].updateValueAndValidity();
+        });
+
+        Object.keys(organizationCtrl.controls).forEach(key => {
+          organizationCtrl.controls[key].setValue(null);
+          organizationCtrl.controls[key].setValidators(null);
+          organizationCtrl.controls[key].updateValueAndValidity();
+        });
+      }
+
+      if (providerType === this.PROVIDER_TYPE.ORGANIZATION) {
+        Object.keys(individualCtrl.controls).forEach(key => {
+          individualCtrl.controls[key].setValue(null);
+          individualCtrl.controls[key].setValidators(null);
+          individualCtrl.controls[key].updateValueAndValidity();
+        });
+
+        Object.keys(organizationCtrl.controls).forEach(key => {
+          organizationCtrl.controls[key].setValidators(this.initProviderOrganizationModel()[key][1]);
+          organizationCtrl.controls[key].updateValueAndValidity();
+        });
+      }
+    });
+  }
+
+  private setProviderType(type: string) {
+    // update payment method type value
+    const ctrl: FormControl = (<any>this.searchProviderFrom).controls.providerType.controls.type;
+    ctrl.setValue(type);
+  }
+
+  isIndividualProviderType() {
+    return this.searchProviderFrom.value.providerType.type === this.PROVIDER_TYPE.INDIVIDUAL;
+  }
+
+  showErrorMessage(formControlName: string) {
+    const formControl = this.searchProviderFrom.get(formControlName);
+    return formControl.invalid && (formControl.dirty || formControl.touched);
+  }
+
+  showProviderTypeErrorMessage(formControlName: string) {
+    if (this.searchProviderFrom.value.providerType.type === this.PROVIDER_TYPE.INDIVIDUAL) {
+      const formControl = (<any>this.searchProviderFrom).controls.providerType.controls.individual.controls[formControlName];
+      return formControl.invalid && (formControl.dirty || formControl.touched);
+    } else {
+      const formControl = (<any>this.searchProviderFrom).controls.providerType.controls.organization.controls[formControlName];
+      return formControl.invalid && (formControl.dirty || formControl.touched);
+    }
   }
 
   resetAccordionTab() {
     this.accordionTab = true;
   }
 
-  reset() {
+  clearForm() {
     this.searchProviderFrom.reset();
+    this.setProviderType(this.PROVIDER_TYPE.INDIVIDUAL);
+  }
+
+  prepareSearchProviders(): ProviderRequestQuery {
+    const formModel = this.searchProviderFrom.value;
+    if (formModel.providerType.type === this.PROVIDER_TYPE.INDIVIDUAL) {
+      return {
+        state: formModel.state,
+        city: formModel.city,
+        zipCode: formModel.zipCode,
+        lastName: formModel.providerType.individual.lastName,
+        firstName: formModel.providerType.individual.firstName,
+        genderCode: formModel.providerType.individual.genderCode,
+        phone: formModel.providerType.individual.phone
+      };
+    } else {
+      return {
+        state: formModel.state,
+        city: formModel.city,
+        zipCode: formModel.zipCode,
+        orgName: formModel.providerType.organization.orgName,
+        phone: formModel.providerType.organization.phone
+      };
+    }
   }
 
   searchProviders() {
-    let requestParams: ProviderRequestQuery = <ProviderRequestQuery>(this.searchProviderFrom.value);
+    let requestParams: ProviderRequestQuery = this.prepareSearchProviders();
 
     this.providerService.searchProviders(requestParams)
       .subscribe(res => {
