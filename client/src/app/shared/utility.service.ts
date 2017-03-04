0a165003c03d115732263f28400c94ac27823a57
JiahaoLi <Jiahao.Li@feisystems.com>
2017-03-04 16:14:03 -0500

    Implement to format zip code in provider address pipe
    #12752: As a developer, I will have created "Provider" angular components for Consent Management UI

--- client/src/app/shared/utility.service.ts
+++ client/src/app/shared/utility.service.ts
@@ -1,13 +1,14 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from "@angular/core";
 import {Router} from "@angular/router";
 import {DatePipe} from "@angular/common";
 
 @Injectable()
 export class UtilityService {
 
-  constructor(private router: Router, private datePipe:DatePipe) { }
+  constructor(private router: Router, private datePipe: DatePipe) {
+  }
 
-  navigateToWithParameters(url: string, params:any) {
+  navigateToWithParameters(url: string, params: any) {
     this.router.navigate([url, params]);
   }
 
@@ -15,15 +16,22 @@ export class UtilityService {
     this.router.navigate([url]);
   }
 
-  removeAll(entries:any[]){
-    entries.splice(0,entries.length);
+  removeAll(entries: any[]) {
+    entries.splice(0, entries.length);
   }
 
-  formatDate(dateStr:string, dateFormat:string){
+  formatDate(dateStr: string, dateFormat: string) {
     return this.datePipe.transform(dateStr, dateFormat);
   }
 
-  isDefined(entity:any): boolean{
+  isDefined(entity: any): boolean {
     return (typeof entity !== 'undefined')
+  }
+
+  formatZipCode(zipCode: string): string {
+    if (zipCode.length > 5) {
+      zipCode = zipCode.slice(0, 5) + "-" + zipCode.slice(5);
+    }
+    return zipCode;
   }
 }
