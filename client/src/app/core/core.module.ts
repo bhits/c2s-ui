d98e205352988dfc4d9e9c51fd98e3bd1d308e3b
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-03-08 10:29:25 -0500

    Implement notification service.
    #12908 - As a developer, I will have integrated the componentized UI with the Backend [3.0.0]

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -4,6 +4,7 @@ import {Http, XHRBackend, RequestOptions} from "@angular/http";
 import {SlimLoadingBarService, SlimLoadingBarModule} from "ng2-slim-loading-bar";
 import {ExceptionService} from "./exception.service";
 import {httpInterceptorServiceFactory} from "./http-interceptor.service";
+import {NotificationService} from "./notification.service";
 
 @NgModule({
   imports: [
@@ -13,13 +14,14 @@ import {httpInterceptorServiceFactory} from "./http-interceptor.service";
   declarations: [],
   providers: [
     ExceptionService,
+    NotificationService,
     {
       provide: Http,
       useFactory: httpInterceptorServiceFactory,
       deps: [XHRBackend, RequestOptions, SlimLoadingBarService]
     }
   ],
-  exports: [SlimLoadingBarModule]
+  exports: [SlimLoadingBarModule, NotificationService]
 })
 export class CoreModule {
 }
