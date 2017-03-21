6d9b9eb6927154638adaa1c8be08f69d0fc6afe2
JiahaoLi <Jiahao.Li@feisystems.com>
2017-03-20 20:38:45 -0400

    Remove duplicate providerName Pipe
    #12908: As a developer, I will have integrated the componentized UI with the Backend [3.0.0]

--- client/src/app/provider/shared/provider-projection-name.pipe.spec.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable:no-unused-variable */
-import {ProviderProjectionNamePipe} from "./provider-projection-name.pipe";
-
-describe('ProviderProjectionNamePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ProviderProjectionNamePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
