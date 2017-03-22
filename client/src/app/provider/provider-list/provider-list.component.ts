62d5b35ecf5b2c3d6bca245cd2e5e8098e5ec71d
Joe Morel <joe.morel@feisystems.com>
2017-03-21 21:59:52 -0400

    Edit spacing on Home page. Edit header on Provider List page. Change css to scss.
    #12906 - As a developer, I will have designed and applied the styling to C2S UI [3.0.0]

--- client/src/app/provider/provider-list/provider-list.component.ts
+++ client/src/app/provider/provider-list/provider-list.component.ts
@@ -7,7 +7,7 @@ import {DataService} from "../../shared/data.service";
 @Component({
   selector: 'c2s-provider-list',
   templateUrl: './provider-list.component.html',
-  styleUrls: ['./provider-list.component.css']
+  styleUrls: ['provider-list.component.scss']
 })
 
 export class ProviderListComponent implements OnInit {
