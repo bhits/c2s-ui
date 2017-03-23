ca2ad0c1ddcdf2815762ea345f38f459f1612a76
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-03-23 18:40:43 -0400

    Update the model to match DTOs
    #12908 - As a developer, I will have integrated the componentized UI with the Backend [3.0.0]

--- client/src/app/consent/shared/sensitivity-policy.ts
+++ client/src/app/consent/shared/sensitivity-policy.ts
@@ -3,8 +3,9 @@ import {SensitivityPolicyCode} from "./sensitivity-policy-code.enum";
 
 export class SensitivityPolicy {
   code: SensitivityPolicyCode;
-  description:string;
-  displayName:string;
-  displayOrder:number;
-  federal:boolean;
+  description: string;
+  displayName: string;
+  displayOrder: number;
+  federal: boolean;
+  system: string;
 }
