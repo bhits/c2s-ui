c7ef6861f1c8601b7b202b41bb0df136cdcc472a
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-03-31 12:49:41 -0400

    Upgrade karma and protractor config in relation to angular-cli update
    #13206 - Create a consent :: Share information is still negative

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -9,8 +9,12 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+      require('karma-jasmine-html-reporter'),
     ],
+    client:{
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
     files: [
       { pattern: './src/test.ts', watched: false }
     ],
@@ -30,7 +34,7 @@ module.exports = function (config) {
     },
     reporters: config.angularCli && config.angularCli.codeCoverage
               ? ['progress', 'coverage-istanbul']
-              : ['progress'],
+              : ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
