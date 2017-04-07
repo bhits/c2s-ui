4dd1447fa5fa7322be89ca262f8c54e50284c6fe
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-04-07 09:12:48 -0400

    Use ngx-translate to apply multi-language feature in home page.
    #13249 - As a developer, I will have implemented multiple language support in C2S patient UI backend and front-end

--- client/src/app/home/home/home.component.ts
+++ client/src/app/home/home/home.component.ts
@@ -2,28 +2,39 @@ import {Component, OnInit} from "@angular/core";
 import {DataService} from "../../shared/data.service";
 import {UtilityService} from "../../shared/utility.service";
 
+import {TranslateService} from "@ngx-translate/core";
+
 @Component({
   selector: 'c2s-home',
   templateUrl: './home.component.html',
   styleUrls: ['home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  private providerTranslateParam: any = {value:''} ;
+  private consentTransalteParam: any = {value:''};
   private totalProviders: number = 0;
   private totalConsents: number = 0;
 
   constructor(private dataService: DataService,
-              private utilityService: UtilityService) {
+              private utilityService: UtilityService,
+              private translate: TranslateService) {
   }
 
   ngOnInit() {
     this.dataService.getProviders()
       .subscribe(
-        res => this.totalProviders = res.length,
+        res => {
+          this.totalProviders = res.length;
+          this.providerTranslateParam.value = res.length > 1 ? 's' : ''
+        },
         err => console.log(err)
       );
     this.dataService.getConsents(0)
       .subscribe(
-        consentList => this.totalConsents = consentList.totalElements,
+        consentList => {
+          this.totalConsents = consentList.totalElements;
+          this.consentTransalteParam.value = consentList.totalElements > 1 ? 's' : ''
+        },
         err => console.log(err))
     ;
   }
