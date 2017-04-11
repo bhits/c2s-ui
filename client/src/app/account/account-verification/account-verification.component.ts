af5e7108053b3ca4c93225d44ea0b02e3a76114c
JiahaoLi <Jiahao.Li@feisystems.com>
2017-04-11 14:02:59 -0400

    Implement account verification component
    #13210: As a developer, I will have implemented the Staff Admin UI for registering a patient

---
+++ client/src/app/account/account-verification/account-verification.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from "@angular/core";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'c2s-account-verification',
+  templateUrl: './account-verification.component.html',
+  styleUrls: ['./account-verification.component.scss']
+})
+export class AccountVerificationComponent implements OnInit {
+  public accountVerificationFrom: FormGroup;
+  public FORMAT: string = "MM/DD/YYYY";
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.accountVerificationFrom = this.formBuilder.group({
+      birthDate: ['', Validators.required],
+      verificationCode: ['', [Validators.minLength(2), Validators.required]]
+    });
+  }
+
+  public clear() {
+    this.accountVerificationFrom.reset();
+  }
+}
