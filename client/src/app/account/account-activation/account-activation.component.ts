4510ce25320267783f88a0ee5c07723f44a702f5
JiahaoLi <Jiahao.Li@feisystems.com>
2017-04-28 19:24:13 -0400

    Integrate staff-ui with BFF API for checking duplicate username
    #13489:As a developer, I will have integrated staff UI with back end to register and manage patient users

--- client/src/app/account/account-activation/account-activation.component.ts
+++ client/src/app/account/account-activation/account-activation.component.ts
@@ -8,6 +8,8 @@ import {ValidationRules} from "../../shared/validation-rules.model";
 import {AccountService} from "../shared/account.service";
 import {ValidationService} from "../../shared/validation.service";
 import {AccountActivationResponse} from "../shared/account-activation-response.model";
+import {Subject} from "rxjs/Subject";
+import {NotificationService} from "../../core/notification.service";
 
 @Component({
   selector: 'c2s-account-activation',
@@ -15,6 +17,8 @@ import {AccountActivationResponse} from "../shared/account-activation-response.m
   styleUrls: ['./account-activation.component.scss']
 })
 export class AccountActivationComponent implements OnInit {
+  private usernameTerms = new Subject<string>();
+  public isUsernameUsed: boolean;
   public accountActivationFrom: FormGroup;
   public passwordErrorMessage: string = ValidationRules.PASSWORD_MESSAGE;
   public mismatchedPasswordsMessage: string = ValidationRules.MISMATCHED_PASSWORDS_MESSAGE;
@@ -23,6 +27,7 @@ export class AccountActivationComponent implements OnInit {
               private accountVerificationService: AccountVerificationService,
               private c2sUiApiUrlService: C2sUiApiUrlService,
               private formBuilder: FormBuilder,
+              private notificationService: NotificationService,
               private validationService: ValidationService,
               private utilityService: UtilityService) {
   }
@@ -33,10 +38,26 @@ export class AccountActivationComponent implements OnInit {
       password: ['', [Validators.pattern(ValidationRules.PASSWORD_PATTERN), Validators.required]],
       confirmPassword: ['', Validators.required]
     }, {validator: this.validationService.matchingPasswords('password', 'confirmPassword')});
+
+    this.usernameTerms
+      .debounceTime(300)
+      .distinctUntilChanged()
+      .switchMap(username => this.accountService.checkDuplicateUsername(username))
+      .subscribe(
+        (res) => {
+          this.isUsernameUsed = res.used;
+        },
+        err => {
+          this.notificationService.show("Failed to check username, please try again later...");
+        });
   }
 
   public clear() {
     this.accountActivationFrom.reset();
+  }
+
+  public checkUsername(): void {
+    this.usernameTerms.next(this.accountActivationFrom.value.username);
   }
 
   public activate() {
