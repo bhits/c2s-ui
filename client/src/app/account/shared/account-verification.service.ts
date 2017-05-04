a47d67789871340c899c496525dc79cfd59471db
JiahaoLi <Jiahao.Li@feisystems.com>
2017-05-04 00:27:10 -0400

    Implement to retrieve EmailLinkInfo
    #13489:As a developer, I will have integrated staff UI with back end to register and manage patient users

--- client/src/app/account/shared/account-verification.service.ts
+++ client/src/app/account/shared/account-verification.service.ts
@@ -11,9 +11,19 @@ export class AccountVerificationService {
   constructor(private sessionStorageService: SessionStorageService) {
   }
 
-  public retrieveEmailToken(verificationPath: string): string {
+  public retrieveEmailLinkInfo(verificationPath: string): Map<EmailLinkInfoKey, string> {
     const SEPARATOR: string = "emailToken=";
-    return verificationPath.split(SEPARATOR).pop();
+    let emailLinkInfoValue: string = verificationPath.split(SEPARATOR).pop();
+    if (emailLinkInfoValue != null) {
+      let emailToken: string = emailLinkInfoValue.split("&")[0];
+      let userPreferredLocale: string = emailLinkInfoValue.split("&")[1];
+      return new Map(
+        [
+          [EmailLinkInfoKey.EMAIL_TOKEN, emailToken],
+          [EmailLinkInfoKey.USER_PREFERRED_LOCALE, userPreferredLocale]
+        ]
+      );
+    }
   }
 
   public setUserFullName(activationResponse: AccountActivationResponse): void {
@@ -47,4 +57,9 @@ export class AccountVerificationService {
     }
     return ''
   }
+}
+
+export enum EmailLinkInfoKey {
+  EMAIL_TOKEN,
+  USER_PREFERRED_LOCALE
 }
