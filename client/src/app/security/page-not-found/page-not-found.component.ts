508302456e0bfcc7d63e2be3c8ea0832cae5a75a
JiahaoLi <Jiahao.Li@feisystems.com>
2017-05-08 12:18:24 -0400

    Implement i18n in page not found page
    #13489:As a developer, I will have integrated staff UI with back end to register and manage patient users

--- client/src/app/security/page-not-found/page-not-found.component.ts
+++ client/src/app/security/page-not-found/page-not-found.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {SessionStorageService} from "src/app/security/shared/session-storage.service";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'c2s-page-not-found',
@@ -6,12 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['page-not-found.component.css']
 })
 export class PageNotFoundComponent implements OnInit {
+  private USER_PREFERRED_LOCALE_KEY: string = 'user-preferred-locale';
 
-  title:string = "Page Not Found";
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private sessionStorageService: SessionStorageService,
+              private translate: TranslateService) {
   }
 
+  ngOnInit() {
+    if (this.getUserPreferredLocale() != null) {
+      this.translate.setDefaultLang(this.getUserPreferredLocale());
+    } else {
+      //Set default locale to be English if there is no default locale provided.
+      this.translate.setDefaultLang("en");
+    }
+  }
+
+  private getUserPreferredLocale(): string {
+    return this.sessionStorageService.getItemFromSessionStorage(this.USER_PREFERRED_LOCALE_KEY);
+  }
 }
