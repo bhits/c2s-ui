fa82e77e81b931f3b60d9627078f0a3b05da6626
Shruti Rao <shruti.rao@feisystems.com>
2017-05-15 15:06:15 -0400

    Add Try-Policy to consent stages
    #13763:As a developer, I will have implemented the Try My Policy UI and integrated with backend [v3.0]

--- client/src/app/consent/shared/consent-stages.model.ts
+++ client/src/app/consent/shared/consent-stages.model.ts
@@ -11,6 +11,7 @@ export const CONSENT_STAGES: ConsentStage[] = [
       {key: ConsentStageOptionKey.EDIT, icon: "edit", text: "CONSENTS.CARD.CONSENT_SAVE_STAGE.EDIT_THIS_CONSENT", routerLink: '/consent-create-edit'},
       {key: ConsentStageOptionKey.DOWNLOAD_SAVED_PDF, icon: "search", text: "CONSENTS.CARD.CONSENT_SAVE_STAGE.PREVIEW_THIS_CONSENT", isMethod: true},
       {key: ConsentStageOptionKey.SIGN, icon: "assignment_turned_in", text: "CONSENTS.CARD.CONSENT_SAVE_STAGE.PROVIDE_ESIGNATURE", routerLink: '/consent-sign'},
+      {key: ConsentStageOptionKey.APPLY_TRY_POLICY, icon: "settings", text: "CONSENTS.CARD.CONSENT_SAVE_STAGE.APPLY_TRY_POLICY", routerLink: '/consent-tryPolicy'},
       {key: ConsentStageOptionKey.DELETE, icon: "delete_forever", text: "CONSENTS.CARD.CONSENT_SAVE_STAGE.DELETE_THIS_PROVIDER", isMethod: true}]
   },
   {
