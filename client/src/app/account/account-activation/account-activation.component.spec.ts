9c06f9bc13da1ed13520db28a951098bbeaab74d
Teng Luo <Teng.Luo@feisystems.com>
2017-05-23 11:13:28 -0400

    create account service stub
    #13774 - As a developer, I will have implemented C2S UI Frontend - Unit Testing

--- client/src/app/account/account-activation/account-activation.component.spec.ts
+++ client/src/app/account/account-activation/account-activation.component.spec.ts
@@ -2,23 +2,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-
+import {AccountService} from "../shared/account.service";
 import { AccountActivationComponent } from './account-activation.component';
 
 describe('AccountActivationComponent', () => {
   let component: AccountActivationComponent;
   let fixture: ComponentFixture<AccountActivationComponent>;
 
+  //async before each
   beforeEach(async(() => {
+    // declare account service stub
+    let accountServiceStub={
+
+    };
     TestBed.configureTestingModule({
-      declarations: [ AccountActivationComponent ]
+      declarations: [ AccountActivationComponent ],
+      providers: [{provide: AccountService, useValue: accountServiceStub }]//
     })
     .compileComponents();
   }));
 
+  //synchronous before each
   beforeEach(() => {
     fixture = TestBed.createComponent(AccountActivationComponent);
-    component = fixture.componentInstance;
+    component = fixture.componentInstance;// AccountActivationComponent test instance
     fixture.detectChanges();
   });
 
