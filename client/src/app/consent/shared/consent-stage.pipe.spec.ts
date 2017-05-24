74569d552e2b9d8ee95542dee95ee7682ddba178
Teng Luo <Teng.Luo@feisystems.com>
2017-05-24 09:42:51 -0400

    test pipe transforms, not done yet
    #13774 - As a developer, I will have implemented C2S UI Frontend - Unit Testing

--- client/src/app/consent/shared/consent-stage.pipe.spec.ts
+++ client/src/app/consent/shared/consent-stage.pipe.spec.ts
@@ -4,8 +4,23 @@ import { TestBed, async } from '@angular/core/testing';
 import { ConsentStagePipe } from './consent-stage.pipe';
 
 describe('ConsentStagePipe', () => {
+
+  // Arrange
+  let pipe = new ConsentStagePipe();
+  type ArgType = "text" | "icon" | "color";
+  let argTypeTest : ArgType ;
+  let testValue : any;
+  // Assert
   it('create an instance', () => {
-    const pipe = new ConsentStagePipe();
+    //const pipe = new ConsentStagePipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('pipe transforms', () => {
+    //const pipe = new ConsentStagePipe();
+
+    expect(pipe.transform(testValue,"text")).toBe("test");
+  });
+
+
 });
