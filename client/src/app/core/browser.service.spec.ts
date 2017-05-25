5799a216ee94a8b73d98f7ba0b75133dc79f3105
Teng Luo <Teng.Luo@feisystems.com>
2017-05-25 17:04:55 -0400

    Test browserServcice and isChrome isFireFox isIE and isSafari
    #13774 - As a developer, I will have implemented C2S UI Frontend - Unit Testing

--- client/src/app/core/browser.service.spec.ts
+++ client/src/app/core/browser.service.spec.ts
@@ -3,6 +3,9 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 import { BrowserService } from './browser.service';
 
+
+
+
 describe('BrowserService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -13,4 +16,28 @@ describe('BrowserService', () => {
   it('should ...', inject([BrowserService], (service: BrowserService) => {
     expect(service).toBeTruthy();
   }));
+
 });
+
+describe('BrowserService without the TestBed', () => {
+  let browserService :  BrowserService;
+
+  beforeEach(() => {browserService = new BrowserService();});
+
+  it('#isChrome',()=>{
+    expect(browserService.isChrome()).toBe(true);
+  });
+  it('#isFireFox',()=>{
+    expect(browserService.isFireFox()).toBe(false);
+  });
+  it('#isIE',()=>{
+    expect(browserService.isIE()).toBe(false);
+  });
+  it('#isSafari',()=>{
+    expect(browserService.isSafari()).toBe(false);
+  });
+  it('#detectBrowser should return chrome for tester browser',()=>{
+    expect(browserService.detectBrowser()).toBe('chrome');
+  });
+});
+
