099f705a0fb3676fec0da1bb200f81cc22693479
Teng Luo <Teng.Luo@feisystems.com>
2017-06-01 15:20:46 -0400

    add c2s-ui-api-url service
    #13774 - As a developer, I will have implemented C2S UI Frontend - Unit Testing

--- client/src/app/shared/c2s-ui-api-url.service.spec.ts
+++ client/src/app/shared/c2s-ui-api-url.service.spec.ts
@@ -14,3 +14,48 @@ describe('C2sUiApiUrlService', () => {
     expect(service).toBeTruthy();
   }));
 });
+
+describe('C2sUiApiUrlService', () => {
+  let service;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [C2sUiApiUrlService]
+    });
+  });
+
+  beforeEach(inject([C2sUiApiUrlService], s => {
+    service = s;
+  }));
+  it('should get loginUrl, test getLoginUrl() work', () => {
+    expect(service.getLoginUrl()).toContain(service.urls.get('loginUrl'));
+  });
+  it('should get accountActivationUrl, test getAccountActivationUrl():  work', () => {
+    expect(service. getAccountActivationUrl()).toContain(service.urls.get('accountActivationUrl'));
+  });
+  it('should get accountActivationSuccessUrl, test getAccountActivationSuccessUrl():  work', () => {
+    expect(service.getAccountActivationSuccessUrl()).toContain(service.urls.get('accountActivationSuccessUrl'));
+  });
+  it('should get accountActivationErrorUrl, test getAccountActivationErrorUrl():  work', () => {
+    expect(service.getAccountActivationErrorUrl()).toContain(service.urls.get('accountActivationErrorUrl'));
+  });
+  it('should get UmsBaseUrl, test UmsBaseUrl():  work', () => {
+    expect(service.getUmsBaseUrl()).toContain(service.urls.get('UmsBaseUrl'));
+  });
+  it('should get PcmBaseUrl, test getPcmBaseUrl():  work', () => {
+    expect(service.getPcmBaseUrl()).toContain(service.urls.get('PcmBaseUrl'));
+  });
+  it('should get PlsBaseUrl, test getPlsBaseUrl():  work', () => {
+    expect(service.getPlsBaseUrl()).toContain(service.urls.get('PlsBaseUrl'));
+  });
+  it('should get VssBaseUrl, test getVssBaseUrl():  work', () => {
+    expect(service.getVssBaseUrl()).toContain(service.urls.get('VssBaseUrl'));
+  });
+  it('should get PhrBaseUrl, test getPhrBaseUrl():  work', () => {
+    expect(service.getPhrBaseUrl()).toContain(service.urls.get('PhrBaseUrl'));
+  });
+  it('should get TryPolicyBaseUrl, test getTryPolicyBaseUrl()):  work', () => {
+    expect(service.getTryPolicyBaseUrl()).toContain(service.urls.get('TryPolicyBaseUrl'));
+  });
+
+});
