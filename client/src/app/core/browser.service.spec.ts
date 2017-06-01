a4a1580e0ce4a65efdfd94798da01ad2cd7fffd6
Teng Luo <Teng.Luo@feisystems.com>
2017-06-01 13:32:12 -0400

    added browser detected based on chrome
    #13774 - As a developer, I will have implemented C2S UI Frontend - Unit Testing

--- client/src/app/core/browser.service.spec.ts
+++ client/src/app/core/browser.service.spec.ts
@@ -54,42 +54,6 @@ describe('Service: BrowserService, test based on Chrome browser', () => {
   });
 });
 
-describe('Service: BrowserService, test based on FireFox browser', () => {
-  let service;
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [BrowserService]
-    });
-  });
 
-  beforeEach(inject([BrowserService], s => {
-    service = s;
-  }));
-
-  it('should detect browser, detectBrwoser()',()=>{
-    let detectedBrowser = service.detectBrowser();
-    expect(detectedBrowser).toContain('chrome');
-  });
-
-  it('should be the chrome browser, isChrome()',()=>{
-    let detectedChromeBrowser = service.isChrome();
-    expect(detectedChromeBrowser).toBe(true);
-  });
-
-  it('should not be the FireFox browser, isFireFox()',()=>{
-    let detectedChromeBrowser = service.isFireFox();
-    expect(detectedChromeBrowser).not.toBe(true);
-  });
-
-  it('should not be the IE browser, isIE()',()=>{
-    let detectedChromeBrowser = service.isIE();
-    expect(detectedChromeBrowser).not.toBe(true);
-  });
-
-  it('should not be the Safari browser, isSafari()',()=>{
-    let detectedChromeBrowser = service.isSafari();
-    expect(detectedChromeBrowser).not.toBe(true);
-  });
-});
 
 
