f97ba63b077df56120bd05c66f01a8d992c192c2
Teng Luo <Teng.Luo@feisystems.com>
2017-06-07 17:31:15 -0400

    add unit test for us-phone-number pipe
    #13774 - As a developer, I will have implemented C2S UI Frontend - Unit Testing

--- client/src/app/shared/us-phone-number.pipe.spec.ts
+++ client/src/app/shared/us-phone-number.pipe.spec.ts
@@ -1,9 +1,28 @@
-/* tslint:disable:no-unused-variable */
-import {UsPhoneNumberPipe} from "./us-phone-number.pipe";
+import { UsPhoneNumberPipe } from './us-phone-number.pipe';
 
-describe('UsPhoneNumberPipe', () => {
-  it('create an instance', () => {
-    const pipe = new UsPhoneNumberPipe();
+describe('SensitivityCategoryPipe', () => {
+  let pipe;
+  var validUsPhoneNumber = '1234567890';
+
+  beforeEach(()=>{
+    pipe = new UsPhoneNumberPipe();
+  });
+
+  it('should create an instance', () => {
     expect(pipe).toBeTruthy();
+  });
+
+  it('should return format valid phone number, with input string with digits only and length of string should be 10',()=>{
+    expect(pipe.transform('1234567890')).toBe('(123) 456-7890');
+  });
+
+  it('should not format if input string length not equal 10',()=>{
+    expect(pipe.transform('123456789')).toBe('123456789');
+    expect(pipe.transform('12345678901')).toBe('12345678901');
+  });
+
+  it('should not format if input string cant covert to number',()=>{
+    expect(pipe.transform('teststring')).toBe('teststring');
+    expect(pipe.transform('!@#$%^&*()')).toBe('!@#$%^&*()');
   });
 });
