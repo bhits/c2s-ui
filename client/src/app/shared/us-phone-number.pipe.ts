1dc9cf165522966c84a741259f1e0db65573644c
Teng Luo <Teng.Luo@feisystems.com>
2017-06-07 17:03:02 -0400

    add input check on if,else statement
    #13774 - As a developer, I will have implemented C2S UI Frontend - Unit Testing
    Only format phone number with 10 number input

--- client/src/app/shared/us-phone-number.pipe.ts
+++ client/src/app/shared/us-phone-number.pipe.ts
@@ -11,8 +11,8 @@ export class UsPhoneNumberPipe implements PipeTransform {
   transform(value: string, args?: any): any {
     let phoneNumber = value.toString().trim().replace(/^\+/, '');
     let areaCode, digitNumber;
-
-    if (phoneNumber.length == 10) {
+    console.log(Number(phoneNumber));
+    if (phoneNumber.length == 10 && Number(phoneNumber)) {
       areaCode = phoneNumber.slice(0, 3);
       digitNumber = phoneNumber.slice(3);
     } else {
@@ -21,3 +21,5 @@ export class UsPhoneNumberPipe implements PipeTransform {
     return (" (" + areaCode + ") " + digitNumber.slice(0, 3) + '-' + digitNumber.slice(3)).trim();
   }
 }
+
+
