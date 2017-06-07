757f54419c05a6836db1dd4b2a3b7cd3cdcf14b6
Teng Luo <Teng.Luo@feisystems.com>
2017-06-06 20:36:59 -0400

    add unit test for consent-stage pipe
    #13774 - As a developer, I will have implemented C2S UI Frontend - Unit Testing

--- client/src/app/consent/shared/consent-stage.pipe.spec.ts
+++ client/src/app/consent/shared/consent-stage.pipe.spec.ts
@@ -1,26 +1,47 @@
-/* tslint:disable:no-unused-variable */
+import {ConsentStagePipe} from "./consent-stage.pipe";
 
-import { TestBed, async } from '@angular/core/testing';
-import { ConsentStagePipe } from './consent-stage.pipe';
 
 describe('ConsentStagePipe', () => {
+  let pipe;
 
-  // Arrange
-  let pipe = new ConsentStagePipe();
-  type ArgType = "text" | "icon" | "color";
-  let argTypeTest : ArgType ;
-  let testValue : any;
-  // Assert
+  beforeEach(()=>{
+    pipe = new ConsentStagePipe();
+  });
+
   it('create an instance', () => {
-    //const pipe = new ConsentStagePipe();
     expect(pipe).toBeTruthy();
   });
 
-  it('pipe transforms', () => {
-    //const pipe = new ConsentStagePipe();
-
-    expect(pipe.transform(testValue,"text")).toBe("test");
+  it('check transform with null value to be undefined', () => {
+    expect(pipe.transform(null)).toBeUndefined();
+  });
+  it('check transform without argType to be undefined', () => {
+    expect(pipe.transform('')).toBeUndefined();
+    expect(pipe.transform('SIGNED')).toBeUndefined();
   });
 
+  it('check transform with vaild value, and argType with icon/text/color to be defined', () => {
+    expect(pipe.transform('SIGNED','text')).toBeDefined();
+    expect(pipe.transform('SIGNED','icon')).toBeDefined();
+    expect(pipe.transform('SIGNED','color')).toBeDefined();
+  });
+
+  it('check transform with SAVED value', () => {
+    expect(pipe.transform('SAVED','text')).toBe('CONSENTS.CARD.CONSENT_SAVE_STAGE.IN_PROGRESS');
+    expect(pipe.transform('SAVED','icon')).toBe('edit');
+    expect(pipe.transform('SAVED','color')).toBe('accent');
+  });
+
+  it('check transform with SIGNED value', () => {
+    expect(pipe.transform('SIGNED','text')).toBe('CONSENTS.CARD.CONSENT_SIGNED_STAGE.SIGNED');
+    expect(pipe.transform('SIGNED','icon')).toBe('check_circle');
+    expect(pipe.transform('SIGNED','color')).toBe('primary');
+  });
+
+  it('check transform with REVOKED value', () => {
+    expect(pipe.transform('REVOKED','text')).toBe('CONSENTS.CARD.CONSENT_REVOKED_STAGE.REVOKED');
+    expect(pipe.transform('REVOKED','icon')).toBe('cancel');
+    expect(pipe.transform('REVOKED','color')).toBe('warn');
+  });
 
 });
