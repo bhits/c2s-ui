6a203ab51184c1e7cbf88bbd728159160893cf7f
Teng Luo <Teng.Luo@feisystems.com>
2017-06-07 11:30:28 -0400

    Rework unit test
    #13774 - As a developer, I will have implemented C2S UI Frontend - Unit Testing
    create const, add valid value test

--- client/src/app/consent/shared/consent-stage.pipe.spec.ts
+++ client/src/app/consent/shared/consent-stage.pipe.spec.ts
@@ -1,47 +1,73 @@
 import {ConsentStagePipe} from "./consent-stage.pipe";
 
-
 describe('ConsentStagePipe', () => {
   let pipe;
+  const SIGNED = 'SIGNED';
+  const SAVED = 'SAVED';
+  const REVOKED ='REVOKED';
+  const TEXT = 'text';
+  const ICON = 'icon';
+  const COLOR = 'color';
+  var validValue ='SIGNED';
+  var validArgType = 'text';
+  var invalidValueString = 'testValue';
+  var invalidValueNumber = 1;
+  var inValidValueObject = new Object();
+  var inValidArgType = 'invalidArgType';
 
   beforeEach(()=>{
     pipe = new ConsentStagePipe();
   });
 
-  it('create an instance', () => {
+  it('should create an instance', () => {
     expect(pipe).toBeTruthy();
   });
 
-  it('check transform with null value to be undefined', () => {
+  it('should to be undefined to input null value', () => {
     expect(pipe.transform(null)).toBeUndefined();
   });
-  it('check transform without argType to be undefined', () => {
-    expect(pipe.transform('')).toBeUndefined();
-    expect(pipe.transform('SIGNED')).toBeUndefined();
+  // test wrong value
+  it('should to be undefined with input invalid value',()=>{
+    expect(pipe.transform(invalidValueString,validArgType)).toBeUndefined();
+    expect(pipe.transform(invalidValueNumber,validArgType)).toBeUndefined();
+    expect(pipe.transform(inValidValueObject,validArgType)).toBeUndefined();
+    expect(pipe.transform('',validArgType)).toBeUndefined();
+    expect(pipe.transform(null,validArgType)).toBeUndefined();
   });
 
-  it('check transform with vaild value, and argType with icon/text/color to be defined', () => {
-    expect(pipe.transform('SIGNED','text')).toBeDefined();
-    expect(pipe.transform('SIGNED','icon')).toBeDefined();
-    expect(pipe.transform('SIGNED','color')).toBeDefined();
+  it('should to be undefined with input invalid argType', () => {
+    expect(pipe.transform(validValue,inValidArgType)).toBeUndefined();
+    expect(pipe.transform(validValue,null)).toBeUndefined();
+    expect(pipe.transform(validValue,'')).toBeUndefined();
   });
 
-  it('check transform with SAVED value', () => {
+  it('should to transform with valid value, and argType with icon/text/color to be defined', () => {
+    expect(pipe.transform(validValue,TEXT)).toBeDefined();
+    expect(pipe.transform(validValue,ICON)).toBeDefined();
+    expect(pipe.transform(validValue,COLOR)).toBeDefined();
+  });
+
+  it('should to transform with valid argType with SAVAED/SIGNED/REVOKED value to be defined', () => {
+    expect(pipe.transform(SAVED,validArgType)).toBeDefined();
+    expect(pipe.transform(SIGNED,validArgType)).toBeDefined();
+    expect(pipe.transform(REVOKED,validArgType)).toBeDefined();
+  });
+
+  it('should to transform with SAVED value', () => {
     expect(pipe.transform('SAVED','text')).toBe('CONSENTS.CARD.CONSENT_SAVE_STAGE.IN_PROGRESS');
     expect(pipe.transform('SAVED','icon')).toBe('edit');
     expect(pipe.transform('SAVED','color')).toBe('accent');
   });
 
-  it('check transform with SIGNED value', () => {
+  it('should to transform with SIGNED value', () => {
     expect(pipe.transform('SIGNED','text')).toBe('CONSENTS.CARD.CONSENT_SIGNED_STAGE.SIGNED');
     expect(pipe.transform('SIGNED','icon')).toBe('check_circle');
     expect(pipe.transform('SIGNED','color')).toBe('primary');
   });
 
-  it('check transform with REVOKED value', () => {
+  it('should to transform with REVOKED value', () => {
     expect(pipe.transform('REVOKED','text')).toBe('CONSENTS.CARD.CONSENT_REVOKED_STAGE.REVOKED');
     expect(pipe.transform('REVOKED','icon')).toBe('cancel');
     expect(pipe.transform('REVOKED','color')).toBe('warn');
   });
-
 });
