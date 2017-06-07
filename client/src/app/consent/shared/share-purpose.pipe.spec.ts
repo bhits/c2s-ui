6309465cd6fa3ef8b42d9a7db258be8b1a6d9784
Teng Luo <Teng.Luo@feisystems.com>
2017-06-07 14:35:48 -0400

    add unit test for share-purpose pipe
    #13774 - As a developer, I will have implemented C2S UI Frontend - Unit Testing

--- client/src/app/consent/shared/share-purpose.pipe.spec.ts
+++ client/src/app/consent/shared/share-purpose.pipe.spec.ts
@@ -1,35 +1,76 @@
-/* tslint:disable:no-unused-variable */
 import {SharePurposePipe} from "./share-purpose.pipe";
-import { Pipe, PipeTransform } from '@angular/core';
 import {SharePurpose} from "./share-purpose.model";
 import {Identifier} from "../../shared/identifier.model";
+
 describe('SharePurposePipe', () => {
-  it('create an instance', () => {
-    const pipe = new SharePurposePipe();
+  let pipe,testSharePurpose, testIdentifier;
+  const DISPLAY = 'display';
+  const DESCRIPTION = 'description';
+  const SYSTEM = 'system';
+  const VALUE = 'value';
+  var validValue;
+  var validArgType = DISPLAY;
+  var invalidValueString = 'testValue';
+  var invalidValueNumber = 1;
+  var inValidValueObject = new Object();
+  var inValidArgType = 'invalidArgType';
+
+  beforeEach(()=>{
+    pipe = new SharePurposePipe();
+    testIdentifier = new Identifier('System Test','Value Test');
+    testSharePurpose = {description: 'Description Test',
+      display: 'Display Test',
+      identifier: testIdentifier};
+    validValue = testSharePurpose;
+  });
+
+  it('should create an instance', () => {
     expect(pipe).toBeTruthy();
   });
 
-  let pipe = new SharePurposePipe();
-  let mockSharePurpose = new SharePurpose();
-  let mockIdentifer = new Identifier('system test','value test');
-  mockSharePurpose = {description: 'description Test',
-  display: 'Display Test',
-  id: 1,
-  identifier: mockIdentifer};
+  it('should to be undefined to input null value',()=>{
+    expect(pipe.transform(null)).toBeUndefined();
+  });
 
-  it('transforms undefined', () => {
-    expect(pipe.transform(mockSharePurpose)).toBeUndefined();
+  it('should to be undefined when miss value',()=>{
+    expect(pipe.transform()).toBeUndefined();
   });
-  it('transforms case display', () => {
-    expect(pipe.transform(mockSharePurpose,"display")).toBe('Display Test');
+
+  it('should to be undefined when miss argType',()=>{
+    expect(pipe.transform(validValue)).toBeUndefined();
+    expect(pipe.transform(validValue,validArgType)).toBeDefined();
   });
-  it('transforms case description', () => {
-    expect(pipe.transform(mockSharePurpose,"description")).toBe('description Test');
+
+  it('should to be undefined to input invalid value',()=>{
+    expect(pipe.transform('',validArgType)).toBeUndefined();
+    expect(pipe.transform(null,inValidArgType)).toBeUndefined();
+    expect(pipe.transform(invalidValueString,validArgType)).toBeUndefined();
+    expect(pipe.transform(invalidValueNumber,validArgType)).toBeUndefined();
+    expect(pipe.transform(inValidValueObject,validArgType)).toBeUndefined();
   });
-  it('transforms case identifier system', () => {
-    expect(pipe.transform(mockSharePurpose,"system")).toBe('system test');
+
+  it('should undefined when input unknown argType',()=>{
+    expect(pipe.transform(validValue,'')).toBeUndefined();
+    expect(pipe.transform(validValue,null)).toBeUndefined();
+    expect(pipe.transform(validValue,inValidArgType)).toBeUndefined();
   });
-  it('transforms case identifier value', () => {
-    expect(pipe.transform(mockSharePurpose,"value")).toBe('value test');
+
+  it('should be defined with transform valid value, and argType with display/description/system/value', () => {
+    expect(pipe.transform(validValue,DISPLAY)).toBeDefined();
+    expect(pipe.transform(validValue,DESCRIPTION)).toBeDefined();
+    expect(pipe.transform(validValue,SYSTEM)).toBeDefined();
+    expect(pipe.transform(validValue,VALUE)).toBeDefined();
   });
+
+  it('should be defined with transform valid argType and valid value', () => {
+    expect(pipe.transform(validValue,validArgType)).toBeDefined();
+  });
+
+  it('should to get expect from pipe transform with valid value and valid argType', () => {
+    expect(pipe.transform(validValue,DISPLAY)).toBe('Display Test');
+    expect(pipe.transform(validValue,DESCRIPTION)).toBe('Description Test');
+    expect(pipe.transform(validValue,SYSTEM)).toBe('System Test');
+    expect(pipe.transform(validValue,VALUE)).toBe('Value Test');
+  });
+
 });
