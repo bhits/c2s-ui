ab070b06429bed1d8f21ce2012d799619cf65bd3
Teng Luo <Teng.Luo@feisystems.com>
2017-06-08 11:28:52 -0400

    add unit test to provide-name pipe
    #13774 - As a developer, I will have implemented C2S UI Frontend - Unit Testing

--- client/src/app/provider/shared/provider-name.pipe.spec.ts
+++ client/src/app/provider/shared/provider-name.pipe.spec.ts
@@ -1,9 +1,46 @@
-/* tslint:disable:no-unused-variable */
 import {ProviderNamePipe} from "./provider-name.pipe";
+import {FlattenedSmallProvider} from "../../shared/flattened-small-provider.model";
 
 describe('ProviderNamePipe', () => {
-  it('create an instance', () => {
+  let pipe,testFlattenedSmallProvider;
+  const INDIVIDUAL = 'Individual';
+  const ORGANIZATION = 'Organization';
+  var validArgType = INDIVIDUAL;
+  var inValidArgType = 'unknownType';
+
+  beforeEach(()=>{
+    pipe = new ProviderNamePipe();
+    testFlattenedSmallProvider = new FlattenedSmallProvider();
+    testFlattenedSmallProvider.organizationName = 'TestOrganizationName';
+    testFlattenedSmallProvider.firstName = "TestFirstName";
+    testFlattenedSmallProvider.lastName = "TestLastName";
+  });
+
+  it('should create an instance', () => {
     const pipe = new ProviderNamePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should undefined with input invalid value',()=>{
+    expect(pipe.transform('',validArgType)).toBeUndefined();
+    expect(pipe.transform('test',validArgType)).toBeUndefined();
+    expect(pipe.transform(1,validArgType)).toBeUndefined();
+    expect(pipe.transform(new Object(),validArgType)).toBeUndefined();
+  });
+
+  it('should defined with validArgType and invalidArgType function not depends on ArgType',()=>{
+    testFlattenedSmallProvider.entityTypeDisplayName = INDIVIDUAL;
+    expect(pipe.transform(testFlattenedSmallProvider,validArgType)).toBeDefined();
+    expect(pipe.transform(testFlattenedSmallProvider,inValidArgType)).toBeDefined();
+  });
+
+  it('should get expect from transform with entityTypeDisplayName Individual',()=>{
+    testFlattenedSmallProvider.entityTypeDisplayName = INDIVIDUAL;
+    expect(pipe.transform(testFlattenedSmallProvider)).toBe('TestFirstName TestLastName');
+  });
+
+  it('should get expect from transform with entityTypeDisplayName Organization',()=>{
+    testFlattenedSmallProvider.entityTypeDisplayName = ORGANIZATION;
+    expect(pipe.transform(testFlattenedSmallProvider)).toBe('TestOrganizationName');
   });
 });
