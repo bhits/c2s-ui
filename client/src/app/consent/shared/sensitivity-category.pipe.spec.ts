b390d692462331816559671c200f5ef9a38789a4
Teng Luo <Teng.Luo@feisystems.com>
2017-06-12 09:29:34 -0400

    Change by the reviewed
    #13774 - As a developer, I will have implemented C2S UI Frontend - Unit Testing

--- client/src/app/consent/shared/sensitivity-category.pipe.spec.ts
+++ client/src/app/consent/shared/sensitivity-category.pipe.spec.ts
@@ -2,7 +2,7 @@ import {SensitivityCategoryPipe} from "./sensitivity-category.pipe";
 import {Identifier} from "../../shared/identifier.model";
 
 describe('SensitivityCategoryPipe', () => {
-  let pipe,testShareSensitivityCategory, testIdentifier;
+  let pipe, testShareSensitivityCategory, testIdentifier;
   const DISPLAY = 'display';
   const DESCRIPTION = 'description';
   const SYSTEM = 'system';
@@ -14,13 +14,15 @@ describe('SensitivityCategoryPipe', () => {
   var inValidValueObject = new Object();
   var inValidArgType = 'invalidArgType';
 
-  beforeEach(()=>{
+  beforeEach(() => {
     pipe = new SensitivityCategoryPipe();
-    testIdentifier = new Identifier('System Test','Value Test');
-    testShareSensitivityCategory = {description: 'Description Test',
+    testIdentifier = new Identifier('System Test', 'Value Test');
+    testShareSensitivityCategory = {
+      description: 'Description Test',
       display: 'Display Test',
       id: 1,
-      identifier: testIdentifier};
+      identifier: testIdentifier
+    };
     validValue = testShareSensitivityCategory;
   });
 
@@ -28,48 +30,53 @@ describe('SensitivityCategoryPipe', () => {
     expect(pipe).toBeTruthy();
   });
 
-  it('should to be undefined to input null value',()=>{
+  it('should be undefined with null value parameter', () => {
     expect(pipe.transform(null)).toBeUndefined();
   });
 
-  it('should to be undefined when miss value',()=>{
+  it('should be undefined without value parameter', () => {
     expect(pipe.transform()).toBeUndefined();
   });
 
-  it('should to be undefined when miss argType',()=>{
+  it('should be undefined without argType parameter', () => {
     expect(pipe.transform(validValue)).toBeUndefined();
-    expect(pipe.transform(validValue,validArgType)).toBeDefined();
+    expect(pipe.transform(validValue, validArgType)).toBeDefined();
   });
 
-  it('should to be undefined to input invalid value',()=>{
-    expect(pipe.transform('',validArgType)).toBeUndefined();
-    expect(pipe.transform(null,inValidArgType)).toBeUndefined();
-    expect(pipe.transform(invalidValueString,validArgType)).toBeUndefined();
-    expect(pipe.transform(invalidValueNumber,validArgType)).toBeUndefined();
-    expect(pipe.transform(inValidValueObject,validArgType)).toBeUndefined();
+  it('should be undefined with invalid value parameter', () => {
+    expect(pipe.transform('', validArgType)).toBeUndefined();
+    expect(pipe.transform(null, inValidArgType)).toBeUndefined();
+    expect(pipe.transform(invalidValueString, validArgType)).toBeUndefined();
+    expect(pipe.transform(invalidValueNumber, validArgType)).toBeUndefined();
+    expect(pipe.transform(inValidValueObject, validArgType)).toBeUndefined();
   });
 
-  it('should undefined when input unknown argType',()=>{
-    expect(pipe.transform(validValue,'')).toBeUndefined();
-    expect(pipe.transform(validValue,null)).toBeUndefined();
-    expect(pipe.transform(validValue,inValidArgType)).toBeUndefined();
+  it('should be undefined when argType equals "invalidArgType"', () => {
+    expect(pipe.transform(validValue, inValidArgType)).toBeUndefined();
   });
 
-  it('should be defined with transform valid value, and argType with display/description/system/value', () => {
-    expect(pipe.transform(validValue,DISPLAY)).toBeDefined();
-    expect(pipe.transform(validValue,DESCRIPTION)).toBeDefined();
-    expect(pipe.transform(validValue,SYSTEM)).toBeDefined();
-    expect(pipe.transform(validValue,VALUE)).toBeDefined();
+  it('should be undefined when argType equals null', () => {
+    expect(pipe.transform(validValue, null)).toBeUndefined();
   });
 
-  it('should be defined with transform valid argType and valid value', () => {
-    expect(pipe.transform(validValue,validArgType)).toBeDefined();
+  it('should be undefined when argType equals ""', () => {
+    expect(pipe.transform(validValue, '')).toBeUndefined();
   });
 
-  it('should to get expect from pipe transform with valid value and valid argType', () => {
-    expect(pipe.transform(validValue,DISPLAY)).toBe('Display Test');
-    expect(pipe.transform(validValue,DESCRIPTION)).toBe('Description Test');
-    expect(pipe.transform(validValue,SYSTEM)).toBe('System Test');
-    expect(pipe.transform(validValue,VALUE)).toBe('Value Test');
+  it('should transforms "testShareSensitivityCategory,display" to "Display Test"', () => {
+    expect(pipe.transform(validValue, DISPLAY)).toEqual('Display Test');
   });
+
+  it('should transforms "testShareSensitivityCategory,description Test" to "Description Test"', () => {
+    expect(pipe.transform(validValue, DESCRIPTION)).toEqual('Description Test');
+  });
+
+  it('should transforms "testShareSensitivityCategory,system" to "System Test"', () => {
+    expect(pipe.transform(validValue, SYSTEM)).toEqual('System Test');
+  });
+
+  it('should transforms "testShareSensitivityCategory,value" to "System Test"', () => {
+    expect(pipe.transform(validValue, VALUE)).toEqual('Value Test');
+  });
+
 });
