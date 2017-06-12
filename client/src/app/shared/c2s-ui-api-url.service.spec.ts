e0c721df9455e142cc45582610501fb92529abaf
Teng Luo <Teng.Luo@feisystems.com>
2017-06-12 14:49:23 -0400

    Change c2sUiApiUrlService, change toContain to toequal, and change test case phrase
    #13774 - As a developer, I will have implemented C2S UI Frontend - Unit Testing

--- client/src/app/shared/c2s-ui-api-url.service.spec.ts
+++ client/src/app/shared/c2s-ui-api-url.service.spec.ts
@@ -1,19 +1,5 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, async, inject } from '@angular/core/testing';
-import { C2sUiApiUrlService } from './c2s-ui-api-url.service';
-
-describe('C2sUiApiUrlService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [C2sUiApiUrlService]
-    });
-  });
-
-  it('should ...', inject([C2sUiApiUrlService], (service: C2sUiApiUrlService) => {
-    expect(service).toBeTruthy();
-  }));
-});
+import {TestBed, async, inject} from '@angular/core/testing';
+import {C2sUiApiUrlService} from './c2s-ui-api-url.service';
 
 describe('C2sUiApiUrlService', () => {
   let service;
@@ -28,35 +14,43 @@ describe('C2sUiApiUrlService', () => {
     service = s;
   }));
   // test function
-  it('should get loginUrl, test getLoginUrl() work', () => {
-    expect(service.getLoginUrl()).toContain(service.urls.get('loginUrl'));
-  });
-  it('should get accountActivationUrl, test getAccountActivationUrl():  work', () => {
-    expect(service. getAccountActivationUrl()).toContain(service.urls.get('accountActivationUrl'));
-  });
-  it('should get accountActivationSuccessUrl, test getAccountActivationSuccessUrl():  work', () => {
-    expect(service.getAccountActivationSuccessUrl()).toContain(service.urls.get('accountActivationSuccessUrl'));
-  });
-  it('should get accountActivationErrorUrl, test getAccountActivationErrorUrl():  work', () => {
-    expect(service.getAccountActivationErrorUrl()).toContain(service.urls.get('accountActivationErrorUrl'));
-  });
-  it('should get UmsBaseUrl, test UmsBaseUrl():  work', () => {
-    expect(service.getUmsBaseUrl()).toContain(service.urls.get('UmsBaseUrl'));
-  });
-  it('should get PcmBaseUrl, test getPcmBaseUrl():  work', () => {
-    expect(service.getPcmBaseUrl()).toContain(service.urls.get('PcmBaseUrl'));
-  });
-  it('should get PlsBaseUrl, test getPlsBaseUrl():  work', () => {
-    expect(service.getPlsBaseUrl()).toContain(service.urls.get('PlsBaseUrl'));
-  });
-  it('should get VssBaseUrl, test getVssBaseUrl():  work', () => {
-    expect(service.getVssBaseUrl()).toContain(service.urls.get('VssBaseUrl'));
-  });
-  it('should get PhrBaseUrl, test getPhrBaseUrl():  work', () => {
-    expect(service.getPhrBaseUrl()).toContain(service.urls.get('PhrBaseUrl'));
-  });
-  it('should get TryPolicyBaseUrl, test getTryPolicyBaseUrl()):  work', () => {
-    expect(service.getTryPolicyBaseUrl()).toContain(service.urls.get('TryPolicyBaseUrl'));
+  it('should test for loginUrl', () => {
+    expect(service.getLoginUrl()).toEqual(service.urls.get('loginUrl'));
   });
 
+  it('should test for accountActivationUrl', () => {
+    expect(service.getAccountActivationUrl()).toEqual(service.urls.get('accountActivationUrl'));
+  });
+
+  it('should test for accountActivationSuccessUrl', () => {
+    expect(service.getAccountActivationSuccessUrl()).toEqual(service.urls.get('accountActivationSuccessUrl'));
+  });
+
+  it('should test for accountActivationErrorUrl', () => {
+    expect(service.getAccountActivationErrorUrl()).toEqual(service.urls.get('accountActivationErrorUrl'));
+  });
+
+  it('should test for UmsBaseUrl', () => {
+    expect(service.getUmsBaseUrl()).toEqual(service.urls.get('UmsBaseUrl'));
+  });
+
+  it('should test for getPcmBaseUrl', () => {
+    expect(service.getPcmBaseUrl()).toEqual(service.urls.get('PcmBaseUrl'));
+  });
+
+  it('should test for getPlsBaseUrl', () => {
+    expect(service.getPlsBaseUrl()).toEqual(service.urls.get('PlsBaseUrl'));
+  });
+
+  it('should test for VssBaseUrl', () => {
+    expect(service.getVssBaseUrl()).toEqual(service.urls.get('VssBaseUrl'));
+  });
+
+  it('should test for PhrBaseUrl', () => {
+    expect(service.getPhrBaseUrl()).toEqual(service.urls.get('PhrBaseUrl'));
+  });
+
+  it('should test for TryPolicyBaseUrl', () => {
+    expect(service.getTryPolicyBaseUrl()).toEqual(service.urls.get('TryPolicyBaseUrl'));
+  });
 });
