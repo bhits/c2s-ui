e16d081acd963d0cbe040ebaa4200b09c1dc217d
Teng Luo <Teng.Luo@feisystems.com>
2017-06-12 13:42:41 -0400

    Change us-phone number pipe
    #13774 - As a developer, I will have implemented C2S UI Frontend - Unit Testing

--- client/src/app/shared/us-phone-number.pipe.spec.ts
+++ client/src/app/shared/us-phone-number.pipe.spec.ts
@@ -1,13 +1,13 @@
-import { UsPhoneNumberPipe } from './us-phone-number.pipe';
+import {UsPhoneNumberPipe} from './us-phone-number.pipe';
 
-describe('SensitivityCategoryPipe', () => {
+describe('UsPhoneNumberPipe', () => {
   let pipe;
   const validUsPhoneNumber = '1234567890';
-  const invalidUsPhoneNumberLess =  '123456789';  // length less than 10;
-  const invalidUsPhoneNumberMore =  '12345678901';  // length more than 10;
-  const invalidInputString =  'teststring';  // length 10;
-  const invalidInputSymbol =  '!@#$%^&*()';  // length 10;
-  beforeEach(()=>{
+  const invalidUsPhoneNumberLess = '123456789';  // length less than 10;
+  const invalidUsPhoneNumberMore = '12345678901';  // length more than 10;
+  const invalidInputString = 'teststring';  // length euqal 10;
+  const invalidInputSymbol = '!@#$%^&*()';  // length equal 10;
+  beforeEach(() => {
     pipe = new UsPhoneNumberPipe();
   });
 
@@ -15,17 +15,23 @@ describe('SensitivityCategoryPipe', () => {
     expect(pipe).toBeTruthy();
   });
 
-  it('should return format valid phone number, with input string with digits only and length of string should be 10',()=>{
-    expect(pipe.transform(validUsPhoneNumber)).toBe('(123) 456-7890');
+  it('should transforms "1234567890" to "(123) 456-7890"', () => {
+    expect(pipe.transform(validUsPhoneNumber)).toEqual('(123) 456-7890');
   });
 
-  it('should not format if input string length not equal 10',()=>{
-    expect(pipe.transform(invalidUsPhoneNumberLess)).toBe('123456789');
-    expect(pipe.transform(invalidUsPhoneNumberMore)).toBe('12345678901');
+  it('should transforms "123456789" to "123456789"', () => {
+    expect(pipe.transform(invalidUsPhoneNumberLess)).toEqual('123456789');
   });
 
-  it('should not format if input string cant covert to number',()=>{
-    expect(pipe.transform(invalidInputString)).toBe('teststring');
+  it('should transforms "12345678901" to "12345678901"', () => {
+    expect(pipe.transform(invalidUsPhoneNumberMore)).toEqual('12345678901');
+  });
+
+  it('should transforms "teststring" to "teststring"', () => {
+    expect(pipe.transform(invalidInputSymbol)).toBe('!@#$%^&*()');
+  });
+
+  it('should transforms "!@#$%^&*()" to "!@#$%^&*()"', () => {
     expect(pipe.transform(invalidInputSymbol)).toBe('!@#$%^&*()');
   });
 });
