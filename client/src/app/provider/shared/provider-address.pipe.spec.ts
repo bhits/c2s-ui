f19a3c5a8b533ef8f6a93301ea982305dd6b7031
Teng Luo <Teng.Luo@feisystems.com>
2017-06-13 17:01:20 -0400

    Add unit test for providerAddress
    #13774 - As a developer, I will have implemented C2S UI Frontend - Unit Testing
     Used spyOn(Object) for UtitlyService dependencies

--- client/src/app/provider/shared/provider-address.pipe.spec.ts
+++ client/src/app/provider/shared/provider-address.pipe.spec.ts
@@ -1,17 +1,38 @@
-/* tslint:disable:no-unused-variable */
 import {ProviderAddressPipe} from "./provider-address.pipe";
 import {UtilityService} from "../../shared/utility.service";
-import {inject} from "@angular/core/testing";
+import {DatePipe, Location} from "@angular/common";
+import {Router} from "@angular/router";
+import {BrowserService} from "../../core/browser.service";
+import {FlattenedSmallProvider} from "../../shared/flattened-small-provider.model";
 
 describe('ProviderAddressPipe', () => {
-  let utilityService = null;
+  let pipe, testFlattenedSmallProvider;
+  let utilityService: UtilityService;
+  let location: Location;
+  let router: Router;
+  let datePipe: DatePipe;
+  let browserService: BrowserService;
 
-  beforeEach(inject([UtilityService], (utilityService: UtilityService) => {
-    utilityService = utilityService;
-  }));
+  beforeEach(() => {
+    utilityService = new UtilityService(location, router, datePipe, browserService);
+    pipe = new ProviderAddressPipe(utilityService);
+    testFlattenedSmallProvider = new FlattenedSmallProvider();
+    testFlattenedSmallProvider.firstLinePracticeLocationAddress = "streetName";
+    testFlattenedSmallProvider.practiceLocationAddressCityName = "Columbia";
+    testFlattenedSmallProvider.practiceLocationAddressStateName = "MD";
+  });
 
-  it('create an instance', () => {
-    const pipe = new ProviderAddressPipe(utilityService);
-    expect(pipe).toBeTruthy();
+  it('should transforms  to ", , , fakeZipCode"', () => {
+    var spy;
+    spy = spyOn(utilityService, 'formatZipCode').and.returnValue('fakeZipCode')
+    expect(pipe.transform("").toString()).toEqual
+    (', , , fakeZipCode');
+  });
+
+  it('should transforms "streetName, Columbia, MD, 12345" to "streetName, Columbia, MD, 12345"', () => {
+    var spy;
+    spy = spyOn(utilityService, 'formatZipCode').and.returnValue('12345')
+    expect(pipe.transform(testFlattenedSmallProvider).toString()).toEqual
+    ('streetName, Columbia, MD, 12345');
   });
 });
