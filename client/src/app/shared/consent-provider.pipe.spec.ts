b49b5541cff56ea4514f304bbdcb7b252726b1c4
Teng Luo <Teng.Luo@feisystems.com>
2017-06-14 15:30:46 -0400

    Add test for consent-provider
    #13774 - As a developer, I will have implemented C2S UI Frontend - Unit Testing
    Used spyOn(Object) for UtitlyService dependencies
    
    Throw error didnt work

--- client/src/app/shared/consent-provider.pipe.spec.ts
+++ client/src/app/shared/consent-provider.pipe.spec.ts
@@ -7,7 +7,7 @@ import {ConsentProviderPipe} from "./consent-provider.pipe";
 import {Identifier} from "./identifier.model";
 import {Address} from "./address.model";
 
-describe(' ConsentProviderPipe',()=>{
+describe(' ConsentProviderPipe', () => {
   let pipe;
   let utilityService: UtilityService;
   let location: Location;
@@ -15,49 +15,53 @@ describe(' ConsentProviderPipe',()=>{
   let datePipe: DatePipe;
   let browserService: BrowserService;
   let testConsentProvider: ConsentProvider;
-  let testIdentifier1,testIdentifier2: Identifier;
-  let testIdentifierArray : Identifier[];
-  let address : Address;
+  let testIdentifier1, testIdentifier2: Identifier;
+  let testIdentifierArray: Identifier[];
+  let address: Address;
+  let system: string;
 
   beforeEach(() => {
     utilityService = new UtilityService(location, router, datePipe, browserService);
     pipe = new ConsentProviderPipe(utilityService);
     testConsentProvider = new ConsentProvider();
-    testIdentifier1 = new Identifier('FHIR_US_NPI_SYSTEM','test1 identifier value');
-    testIdentifier2 = new Identifier('test2 system','test2 identifier value');
   });
 
-  it('should test for case "npi"',()=>{
-    testIdentifier1 = new Identifier('FHIR_US_NPI_SYSTEM','test1 identifier value');
-    testIdentifier2 = new Identifier('test2 system','test2 identifier value');
-    console.log(testIdentifier1.system);
-    testIdentifierArray = [testIdentifier1,testIdentifier2];
-    console.log(testIdentifierArray);
-    console.log(testIdentifierArray[0].value,testIdentifierArray[0].system);
-
+  it('should test for case "npi"', () => {
+    system = FHIR_US_NPI_SYSTEM;
+    console.log(system);
+    testIdentifier1 = new Identifier(system, 'test1 identifier value');
+    testIdentifier2 = new Identifier('test2 system', 'test2 identifier value');
+    testIdentifierArray = [testIdentifier1, testIdentifier2];
     testConsentProvider.identifiers = testIdentifierArray;
-    console.log(testConsentProvider.identifiers[0].system);
-    expect(pipe.transform(testConsentProvider,'npi')).toEqual("test name");
+    expect(pipe.transform(testConsentProvider, 'npi')).toEqual("test1 identifier value");
   });
 
+  it('should return null with invalid value parameter', () => {
+    expect(pipe.transform(null)).toEqual(null);
+  });
 
-  describe('should test for case "name"',()=>{
-    it('should test for case ORGANIZATION',()=>{
+  describe('should test for case "name"', () => {
+    it('should test for case ORGANIZATION', () => {
       testConsentProvider.providerType = "ORGANIZATION";
       testConsentProvider.name = "test name";
-      expect(pipe.transform(testConsentProvider,'name')).toEqual("test name");
+      expect(pipe.transform(testConsentProvider, 'name')).toEqual("test name");
     });
 
-    it('should test for case PRACTITIONER',()=>{
+    it('should test for case PRACTITIONER', () => {
       testConsentProvider.providerType = "PRACTITIONER";
       testConsentProvider.firstName = "firstName";
       testConsentProvider.middleName = "middleName";
       testConsentProvider.lastName = "lastName";
-      expect(pipe.transform(testConsentProvider,'name')).toEqual("firstName middleName lastName");
+      expect(pipe.transform(testConsentProvider, 'name')).toEqual("firstName middleName lastName");
+    });
+
+    it('should throw error for invalid providerType', () => {
+      testConsentProvider.providerType = "invalidProviderType";
+      expect(pipe.transform(testConsentProvider, 'name')).toThrow(new TypeError("Invalid providerType"));
     });
   });
 
-  it('should test for case "address"',()=>{
+  it('should test for case "address"', () => {
     address = new Address();
     address.line1 = "line1";
     address.line2 = "line2";
@@ -66,14 +70,15 @@ describe(' ConsentProviderPipe',()=>{
     address.postalCode = "12345";
     address.country = "Country";
     testConsentProvider.address = address;
-    expect(pipe.transform(testConsentProvider,'address')).toEqual("line1, line2, city, state, 12345, Country");
+    expect(pipe.transform(testConsentProvider, 'address')).toEqual("line1, line2, city, state, 12345, Country");
   });
 
-  it('should test for case "phone"',()=>{
+  it('should test for case "phone"', () => {
     testConsentProvider.phoneNumber = "1234567890"
-    expect(pipe.transform(testConsentProvider,'phone')).toEqual("1234567890");
+    expect(pipe.transform(testConsentProvider, 'phone')).toEqual("1234567890");
   });
 
-
-
+  it('should test for getName with return ""', () => {
+    expect(pipe.getName(null)).toEqual("");
+  });
 });
