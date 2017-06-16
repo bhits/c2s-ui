52f1c8ab5cab54c989676312b0dce4e8cea19466
Teng Luo <Teng.Luo@feisystems.com>
2017-06-15 22:09:52 -0400

    Change unit test title and add return type test
    #13774 - As a developer, I will have implemented C2S UI Frontend - Unit Testing

--- client/src/app/shared/us-phone-number.pipe.spec.ts
+++ client/src/app/shared/us-phone-number.pipe.spec.ts
@@ -5,7 +5,7 @@ describe('UsPhoneNumberPipe', () => {
   const validUsPhoneNumber = '1234567890';
   const invalidUsPhoneNumberLess = '123456789';  // length less than 10;
   const invalidUsPhoneNumberMore = '12345678901';  // length more than 10;
-  const invalidInputString = 'teststring';  // length euqal 10;
+  const invalidInputString = 'testString';  // length euqal 10;
   const invalidInputSymbol = '!@#$%^&*()';  // length equal 10;
   beforeEach(() => {
     pipe = new UsPhoneNumberPipe();
@@ -15,23 +15,15 @@ describe('UsPhoneNumberPipe', () => {
     expect(pipe).toBeTruthy();
   });
 
-  it('should transforms "1234567890" to "(123) 456-7890"', () => {
+  it('should test for us-phone-number', () => {
     expect(pipe.transform(validUsPhoneNumber)).toEqual('(123) 456-7890');
-  });
-
-  it('should transforms "123456789" to "123456789"', () => {
     expect(pipe.transform(invalidUsPhoneNumberLess)).toEqual('123456789');
-  });
-
-  it('should transforms "12345678901" to "12345678901"', () => {
     expect(pipe.transform(invalidUsPhoneNumberMore)).toEqual('12345678901');
+    expect(pipe.transform(invalidInputSymbol)).toEqual('!@#$%^&*()');
+    expect(pipe.transform(invalidInputString)).toEqual('testString');
   });
 
-  it('should transforms "teststring" to "teststring"', () => {
-    expect(pipe.transform(invalidInputSymbol)).toEqual('!@#$%^&*()');
-  });
-
-  it('should transforms "!@#$%^&*()" to "!@#$%^&*()"', () => {
-    expect(pipe.transform(invalidInputSymbol)).toEqual('!@#$%^&*()');
+  it('should test for us-phone-number return type',()=>{
+    expect(typeof(pipe.transform(validUsPhoneNumber))).toEqual('string');
   });
 });
