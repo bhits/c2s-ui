42692c5f245cfa41f5d0d97767e97daf947b6eab
Michael Hadjiosif <michael.hadjiosif@feisystems.com>
2017-07-03 19:53:37 -0400

    Add 'userId' & 'userAuthId' properties to UmsFullProfileView model
    #14278 - As a developer I will create a profile page for patients to view/update their profile info, with the option of also uploading an avatar

--- client/src/app/user-profile/shared/ums-full-profile-view.model.ts
+++ client/src/app/user-profile/shared/ums-full-profile-view.model.ts
@@ -2,6 +2,8 @@ import {ListOfIdentifiers} from "../../shared/list-of-identifiers.model";
 import {UmsProfileAddress} from "../../shared/ums-profile-address.model";
 
 export class UmsFullProfileView {
+  userId: number;
+  userAuthId: string;
   userLocale: string;
   supportedLocales: string[];
   username: string;
