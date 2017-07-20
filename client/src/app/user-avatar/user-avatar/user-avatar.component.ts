8c261348539e9f187e4e658869b95b9e242773a3
Michael Hadjiosif <michael.hadjiosif@feisystems.com>
2017-07-20 19:00:59 -0400

    Implement saving cropped user avatar image to back-end service
    #14601 - As a developer I will create a profile page for patients to view/update their profile info, with the option of also uploading an avatar

--- client/src/app/user-avatar/user-avatar/user-avatar.component.ts
+++ client/src/app/user-avatar/user-avatar/user-avatar.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit, ViewChild} from "@angular/core";
 import {CropperSettings, ImageCropperComponent} from "ng2-image-cropper";
+import {UserAvatarService} from "../shared/user-avatar.service";
+import {NotificationService} from "../../core/notification.service";
+import {Router} from "@angular/router";
 
 const CROPPED_IMAGE_WIDTH: number = 48;  // Width of cropped avatar image in pixels
 const CROPPED_IMAGE_HEIGHT: number = 48;  // Height of cropped avatar image in pixels
@@ -16,13 +19,17 @@ const CLASS_WHEN_IMAGE_FILE_SELECTED: string = "ready-to-crop";
   styleUrls: ['./user-avatar.component.scss']
 })
 export class UserAvatarComponent implements OnInit {
+  private fileName: string;
+
   data: any;
   cropperSettings: CropperSettings;
 
   @ViewChild('cropper', undefined)
   cropper: ImageCropperComponent;
 
-  constructor() {
+  constructor(private router: Router,
+              private userAvatarService: UserAvatarService,
+              private notificationService: NotificationService) {
     this.cropperSettings = new CropperSettings();
     this.cropperSettings.croppedWidth = CROPPED_IMAGE_WIDTH;
     this.cropperSettings.croppedHeight = CROPPED_IMAGE_HEIGHT;
@@ -32,27 +39,69 @@ export class UserAvatarComponent implements OnInit {
     this.cropperSettings.croppingClass = CLASS_WHEN_IMAGE_FILE_SELECTED;
     this.cropperSettings.noFileInput = true;
     this.data = {};
+    this.fileName = null;
   }
 
   ngOnInit(): void {
   }
 
   submitCroppedImage() {
-    // TODO: Repalce this with call to backend service to save cropped avatar image
-    console.log(this.data);
+    if ((this.data.hasOwnProperty('image')) && (this.data.image != null)) {    // The not-equals operator was deliberately used here instead of the strict not-equals to match both 'null' & 'undefined'
+      if ((this.fileName !== null) && (this.fileName.length > 0)) {
+        this.extractCroppedImageAsFile(this.data.image, this.fileName)
+          .then(file => {
+            this.userAvatarService.saveUserAvatar(UserAvatarService.buildAvatarFileUploadRequest(this.data.image, file, CROPPED_IMAGE_HEIGHT, CROPPED_IMAGE_WIDTH))
+              .subscribe(
+                () => {
+                  this.notificationService.i18nShow("USER_AVATAR.AVATAR_UPLOAD_SUCCESS_MSG");
+                  this.redirectToUserProfile();
+                },
+                (err) => {
+                  console.log(err);
+                  this.notificationService.i18nShow("USER_AVATAR.ERROR_MSGS.ERROR_SAVING_AVATAR");
+                });
+          });
+      }
+    }
+  }
+
+  cancel(): void {
+    this.redirectToUserProfile();
   }
 
   fileChangeListener($event) {
     let image: any = new Image();
     let file: File = $event.target.files[0];
     let myReader: FileReader = new FileReader();
-    let that = this;
+    let currentThis = this;
 
     myReader.onloadend = function (loadEvent: any) {
       image.src = loadEvent.target.result;
-      that.cropper.setImage(image);
+      currentThis.cropper.setImage(image);
     };
 
     myReader.readAsDataURL(file);
+    currentThis.fileName = file.name;
+  }
+
+  private redirectToUserProfile(): void {
+    this.router.navigateByUrl('/user-profile');
+  }
+
+  private extractCroppedImageAsFile(imageSrc: string, fileName: string): Promise<File> {
+    let currentThis = this;
+
+    // separate out the mime component
+    let mimeString = imageSrc.split(',')[0].split(':')[1].split(';')[0];
+
+    return currentThis.srcToFile(imageSrc, fileName, mimeString)
+      .then(file => file);
+  }
+
+  private srcToFile(src, fileName, mimeType): Promise<File> {
+    return (fetch(src)
+        .then(function(res){return res.arrayBuffer();})
+        .then(function(buf){return new File([buf], fileName, {type:mimeType});})
+    );
   }
 }
