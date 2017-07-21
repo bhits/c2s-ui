d84ffc54fef5b7802dd718cc8498f7c003e62ae7
Michael Hadjiosif <michael.hadjiosif@feisystems.com>
2017-07-20 21:15:58 -0400

    Add get avatar image functions & store avatar in session from LimitedProfileService
    #14601 - As a developer I will create a profile page for patients to view/update their profile info, with the option of also uploading an avatar

--- client/src/app/security/shared/limited-profile.service.ts
+++ client/src/app/security/shared/limited-profile.service.ts
@@ -5,20 +5,66 @@ import {C2sUiApiUrlService} from "../../shared/c2s-ui-api-url.service";
 import {Observable} from "rxjs";
 import {UmsLimitedProfile} from "./ums-limited-profile.model";
 import {SessionStorageService} from "./session-storage.service";
+import {AvatarImage} from "../../user-avatar/shared/avatar-image.model";
+import {ExceptionService} from "../../core/exception.service";
+import {isNullOrUndefined} from "util";
 
 @Injectable()
 export class LimitedProfileService {
   private umsLimitedProfileUrl: string = this.c2sUiApiUrlService.getUmsBaseUrl() + "/user/profile";
+  private umsUserAvatarUrl: string = this.c2sUiApiUrlService.getUmsBaseUrl() + "/user-avatars/";
   private UMS_PROFILE_KEY: string = 'c2s-ums-profile';
+  private AVATAR_IMG_KEY: string = 'c2s-avatar-image';
 
   constructor(private http: Http,
               private c2sUiApiUrlService: C2sUiApiUrlService,
-              private sessionStorageService: SessionStorageService) {
+              private sessionStorageService: SessionStorageService,
+              private exceptionService: ExceptionService) {
   }
 
   getUMSProfile(): Observable<UmsLimitedProfile> {
     return this.http.get(this.umsLimitedProfileUrl)
       .map((resp: Response) => <any>(resp.json()));
+  }
+
+  getAvatarImageFromServer(): Observable<AvatarImage> {
+    let umsLimitedProfile: UmsLimitedProfile = this.getProfileFromSessionStorage();
+
+    if (!isNullOrUndefined(umsLimitedProfile)) {
+      const GET_AVATAR_URL: string = this.umsUserAvatarUrl.concat("/user/" + umsLimitedProfile.userId + "/avatar");
+
+      return this.http.get(GET_AVATAR_URL)
+        .map((resp: Response) => <AvatarImage>(resp.json()))
+        .catch(this.exceptionService.handleErrorWithErrorCode);
+    }
+  }
+
+  getAvatarImageFromSessionStorage(): AvatarImage {
+    return this.sessionStorageService.getItemFromSessionStorage(this.AVATAR_IMG_KEY);
+  }
+
+  getAvatarImage(): Observable<AvatarImage> {
+    let avatarImage: AvatarImage = this.getAvatarImageFromSessionStorage();
+
+    if (!isNullOrUndefined(avatarImage)) {
+      console.log(avatarImage);
+      return Observable.of(avatarImage)
+        .map(avatarImage => avatarImage);
+    } else {
+      return this.getAvatarImageFromServer()
+        .do(
+          (data) => {
+            console.log(data);
+            let imgFromServer: AvatarImage = data;
+            this.sessionStorageService.setItemInSessionStorage(this.AVATAR_IMG_KEY, imgFromServer);
+            return imgFromServer;
+          },
+          (err) => {
+            console.log(err);
+            return this.exceptionService.handleErrorWithErrorCode(err);
+          }
+        );
+    }
   }
 
   setProfileInSessionStorage(profile: UmsLimitedProfile) {
