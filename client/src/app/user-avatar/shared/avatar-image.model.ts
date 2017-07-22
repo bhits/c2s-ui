336d03acd06fa69ca3aaf82d0d8e7354bf23cc11
Michael Hadjiosif <michael.hadjiosif@feisystems.com>
2017-07-21 23:23:57 -0400

    Add constructor to AvatarImage model to initialize properties to null
    #14601 - As a developer I will create a profile page for patients to view/update their profile info, with the option of also uploading an avatar

--- client/src/app/user-avatar/shared/avatar-image.model.ts
+++ client/src/app/user-avatar/shared/avatar-image.model.ts
@@ -1,4 +1,11 @@
 export class AvatarImage {
+  constructor() {
+    this.fileContents = null;
+    this.fileExtension = null;
+    this.fileWidthPixels = null;
+    this.fileHeightPixels = null;
+  }
+
   fileContents: string;  // this is a Base64 string
   fileExtension: string;
   fileWidthPixels: number;
