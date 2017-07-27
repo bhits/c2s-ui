20508c5323d44d56059bd1d79d951f3ca8a8a984
Michael Hadjiosif <michael.hadjiosif@feisystems.com>
2017-07-27 17:56:45 -0400

    Add 'CanActivateAuthGuardService' to UserAvatarRoutingModule
    #14601 - As a developer I will create a profile page for patients to view/update their profile info, with the option of also uploading an avatar

--- client/src/app/user-avatar/user-avatar-routing.module.ts
+++ client/src/app/user-avatar/user-avatar-routing.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {UserAvatarComponent} from "./user-avatar/user-avatar.component";
+import {CanActivateAuthGuardService} from "../security/shared/can-activate-auth-guard.service";
 
 const userAvatarRoutes: Routes = [
   {
     path: 'user-avatar',
-    component: UserAvatarComponent
+    component: UserAvatarComponent,
+    canActivate: [CanActivateAuthGuardService]
   }
 ];
 
