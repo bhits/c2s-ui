5374b351cf9f76a3adcb321b8c1194a572dc749b
JiahaoLi <Jiahao.Li@feisystems.com>
2017-08-03 10:54:08 -0400

    Add initial content in health information list page
    #7693: As a patient, I can view my health information from HIE

--- client/src/app/health-information/health-information.module.ts
+++ client/src/app/health-information/health-information.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {healthInformationRoutedComponents, HealthInformationRoutingModule} from './health-information-routing.module';
+import {SharedModule} from "src/app/shared/shared.module";
 
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
     HealthInformationRoutingModule
   ],
   declarations: [healthInformationRoutedComponents]
