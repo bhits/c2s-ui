d02cc75b7252b52ffcc44722071108b55f529bcc
Joe Morel <joe.morel@feisystems.com>
2017-08-08 14:41:09 -0400

    Begin build out of Health Information screen. add initial styling to cda-document-table - In Progress.
    #7693 - As a patient, I can view my health information from HIE

--- client/src/app/health-information/health-information-cda-document-section/health-information-cda-document-section.component.ts
+++ client/src/app/health-information/health-information-cda-document-section/health-information-cda-document-section.component.ts
@@ -1,10 +1,11 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import {PatientHealthDataCdaDocumentSection} from "../shared/patient-health-data-cda-document-section.model";
 
 @Component({
   selector: 'c2s-health-information-cda-document-section',
   templateUrl: './health-information-cda-document-section.component.html',
-  styleUrls: ['./health-information-cda-document-section.component.scss']
+  styleUrls: ['./health-information-cda-document-section.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HealthInformationCdaDocumentSectionComponent implements OnInit {
   @Input()
