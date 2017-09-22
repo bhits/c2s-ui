58c65cc7e7fdb778c47bf3a4057d41526b8b7f04
Tomson M. Ngassa <tomson.ngassa@feisystems.com>
2017-09-22 13:02:33 -0400

    Remove unused code
    #16073 : C2Sv3 :: C2S UI :: As a developer, I will have defined a solution that will reduce the impact of route changes of edge server for UI projects

--- client/src/app/core/shared/shared/common-functions.ts
+++
@@ -1,6 +0,0 @@
-
-export function getBaseLocation() {
-  let paths: string[] = location.pathname.split('/').splice(1, 1);
-  let basePath: string = (paths && paths[0]) || 'c2s-ui'; // Default: c2s-ui
-  return '/' + basePath + '/';
-}
