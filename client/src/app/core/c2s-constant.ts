5d8777379fe90f67aa6875c137ae723d13a06edf
Teng Luo <Teng.Luo@feisystems.com>
2017-10-18 21:48:00 -0400

    Create constant file to store all constant and change session storage key for terms of use agreement
    #14238: As a user, I will be notified when working in a demo system with a notice of disclosure, so that I am reminded of PII constraints.

---
+++ client/src/app/core/c2s-constant.ts
@@ -0,0 +1,13 @@
+export const MenuItemKey = {
+  "HOME": "Home",
+  "ACTIVITY_HISTORY": "Activity History",
+  "PROVIDERS": "Providers",
+  "CONSENTS": "Consents",
+  "MEDICAL_DOCUMENTS": "Medical Documents",
+  "HEALTH_INFORMATION": "Health Information",
+  "MY_PROFILE": "My Profile",
+  "LOGOUT": "Logout"
+}
+export const SessionStorageKey = {
+  "TERMS_OF_USE_AGREEMENT": "c2s-terms-of-use-agreement"
+}
